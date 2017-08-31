9fd0372ac5aeb26f16f1ee794e35531ba421024d
Tony Burdett <tburdett@ebi.ac.uk>
2017-08-31 09:58:41 +0100

    renaming base package

--- src/main/java/org/humancellatlas/ingest/IngestCoreApplication.java
+++ src/main/java/org/humancellatlas/ingest/IngestCoreApplication.java
@@ -1,4 +1,4 @@
-package org.humancellatlas.ingestcore;
+package org.humancellatlas.ingest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/org/humancellatlas/ingest/IngestCoreApplicationTests.java
+++ src/test/java/org/humancellatlas/ingest/IngestCoreApplicationTests.java
@@ -1,4 +1,4 @@
-package org.humancellatlas.ingestcore;
+package org.humancellatlas.ingest;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
