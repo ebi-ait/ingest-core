7d41e5769017ec733e611d93f7aec18d687c48b2
Tony Burdett <tburdett@ebi.ac.uk>
2017-08-31 09:57:37 +0100

    fresh project template

---
+++ src/main/java/org/humancellatlas/ingestcore/IngestCoreApplication.java
@@ -0,0 +1,12 @@
+package org.humancellatlas.ingestcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngestCoreApplication.class, args);
+	}
+}
