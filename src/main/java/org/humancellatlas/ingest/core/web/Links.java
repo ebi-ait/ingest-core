9d713d39d0c026c8e1c6c96d3b76873b98dbdabc
Tony Burdett <tburdett@ebi.ac.uk>
2017-09-05 18:25:16 +0100

    tidying up envelope resource processor

---
+++ src/main/java/org/humancellatlas/ingest/core/web/Links.java
@@ -0,0 +1,13 @@
+package org.humancellatlas.ingest.core.web;
+
+/**
+ * Enumerates the relations that are available in this application to provide some stability across different
+ * implementations. These should not change without serious motivation, as will likely cause backwards-compatibility
+ * breaking changes for clients.
+ *
+ * @author Tony Burdett
+ * @date 05/09/17
+ */
+public class Links {
+    public static final String SUBMIT_REL = "submit";
+}
--- src/main/java/org/humancellatlas/ingest/submission/web/SubmissionEnvelopeResourceProcessor.java
+++ src/main/java/org/humancellatlas/ingest/submission/web/SubmissionEnvelopeResourceProcessor.java
@@ -1,6 +1,7 @@
 package org.humancellatlas.ingest.submission.web;
 
 import lombok.RequiredArgsConstructor;
+import org.humancellatlas.ingest.core.web.Links;
 import org.humancellatlas.ingest.envelope.SubmissionEnvelope;
 import org.springframework.hateoas.EntityLinks;
 import org.springframework.hateoas.Link;
@@ -24,11 +25,11 @@ public class SubmissionEnvelopeResourceProcessor implements ResourceProcessor<Re
     private Link getSubmitLink(SubmissionEnvelope submissionEnvelope) {
         return entityLinks.linkForSingleResource(submissionEnvelope)
                 .slash("/confirmation")
-                .withRel(SubmissionLinks.SUBMIT_REL);
+                .withRel(Links.SUBMIT_REL);
     }
 
-    public Resource<SubmissionEnvelope> process(Resource<SubmissionEnvelope> envelopeResource) {
-        envelopeResource.add(getSubmitLink(envelopeResource.getContent()));
-        return envelopeResource;
+    public Resource<SubmissionEnvelope> process(Resource<SubmissionEnvelope> resource) {
+        resource.add(getSubmitLink(resource.getContent()));
+        return resource;
     }
 }
