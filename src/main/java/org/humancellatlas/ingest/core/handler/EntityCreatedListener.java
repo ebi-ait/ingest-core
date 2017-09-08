550883c361ce5071113edf410929ecee8783c161
Rolando Fernandez <rolando@ebi.ac.uk>
2017-09-08 15:30:47 +0100

    listening for mongo create-events rather than REST create-events when assigning uuids

---
+++ src/main/java/org/humancellatlas/ingest/core/handler/EntityCreatedListener.java
@@ -0,0 +1,31 @@
+package org.humancellatlas.ingest.core.handler;
+
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import org.humancellatlas.ingest.core.AbstractEntity;
+import org.humancellatlas.ingest.core.MetadataDocument;
+import org.humancellatlas.ingest.core.Uuid;
+import org.humancellatlas.ingest.core.service.ValidateMetadataService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
+import org.springframework.data.mongodb.core.mapping.event.BeforeSaveEvent;
+import org.springframework.data.rest.core.annotation.*;
+import org.springframework.data.rest.core.event.AbstractRepositoryEventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+/**
+ * Created by rolando on 06/09/2017.
+ */
+@Component
+public class EntityCreatedListener extends AbstractMongoEventListener<AbstractEntity> {
+
+    @Override
+    public void onBeforeSave(BeforeSaveEvent<AbstractEntity> entityBeforeSaveEvent) {
+        entityBeforeSaveEvent.getSource().setUuid(new Uuid(UUID.randomUUID().toString()));
+    }
+}
+
+
