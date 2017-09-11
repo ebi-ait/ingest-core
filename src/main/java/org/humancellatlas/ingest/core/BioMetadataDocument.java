4243da744bfb19fba1af198540329003a4f78b2c
Rolando Fernandez <rolando@ebi.ac.uk>
2017-09-11 15:40:04 +0100

    added ValidationChecksum property to BioMetadataDocument + checks if validation is required

--- src/main/java/org/humancellatlas/ingest/core/BioMetadataDocument.java
+++ src/main/java/org/humancellatlas/ingest/core/BioMetadataDocument.java
@@ -9,11 +9,13 @@ import lombok.Setter;
 @Getter
 public class BioMetadataDocument extends MetadataDocument {
     private @Setter ValidationStatus validationStatus;
+    private @Setter ValidationChecksum validationChecksum;
     private final Accession accession;
 
     protected BioMetadataDocument(EntityType type, Uuid uuid, SubmissionDate submissionDate, UpdateDate updateDate, Accession accession, Object content, ValidationStatus validationStatus) {
         super(type, uuid, submissionDate, updateDate, content);
         this.accession = accession;
         this.validationStatus = validationStatus;
+        this.validationChecksum = new ValidationChecksum();
     }
 }
--- src/main/java/org/humancellatlas/ingest/core/handler/MetadataDocumentEventHandler.java
+++ src/main/java/org/humancellatlas/ingest/core/handler/MetadataDocumentEventHandler.java
@@ -1,5 +1,6 @@
 package org.humancellatlas.ingest.core.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import org.humancellatlas.ingest.core.*;
@@ -9,8 +10,12 @@ import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventLis
 import org.springframework.data.mongodb.core.mapping.event.AfterSaveEvent;
 import org.springframework.data.rest.core.annotation.*;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.util.StringUtils;
 
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.util.Arrays;
 import java.util.UUID;
 
 /**
@@ -23,10 +28,30 @@ public class MetadataDocumentEventHandler{
     private final @NonNull ValidateMetadataService validateMetadataService;
     private final @NonNull AccessionMetadataService accessionMetadataService;
 
+    @HandleAfterCreate
+    public void onAfterCreate(BioMetadataDocument document){
+        doValidation(document);
+        doAccession(document);
+    }
+
     @HandleAfterSave
     public void onAfterSave(BioMetadataDocument document){
         doValidation(document);
         doAccession(document);
+    }
+
+    @HandleBeforeCreate
+    public void onBeforeCreate(BioMetadataDocument document) {
+        if (! validChecksum(document)) {
+            doValidation(document);
+        }
+    }
+
+    @HandleBeforeSave
+    public void handleBeforeSave(BioMetadataDocument document) {
+        if (! validChecksum(document)) {
+            doValidation(document);
+        }
     }
 
     private void doValidation(BioMetadataDocument document){
@@ -37,6 +62,21 @@ public class MetadataDocumentEventHandler{
     private void doAccession(BioMetadataDocument document) {
         if(StringUtils.isEmpty(document.getAccession()) && document.getValidationStatus().equals(ValidationStatus.VALID)) {
             accessionMetadataService.accessionMetadata(document);
+        }
+    }
+
+    private boolean validChecksum(BioMetadataDocument document) {
+        try {
+            if(document.getContent() != null) {
+                byte[] contentAsBytes = new ObjectMapper().writeValueAsString(document.getContent()).getBytes();
+
+                return Arrays.equals(DigestUtils.md5Digest(contentAsBytes), document.getValidationChecksum().getMd5().getBytes());
+            }
+
+            return  false;
+        } catch (IOException e){
+            // TODO log
+            return false;
         }
     }
 }
