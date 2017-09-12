370236c161c944a07e2a51d0668233a4af03ac45
Tony Burdett <tburdett@ebi.ac.uk>
2017-09-12 13:59:26 +0100

    moving all state code to state package

--- src/main/java/org/humancellatlas/ingest/submission/state/SubmissionEnvelopeStateChangeListener.java
+++ src/main/java/org/humancellatlas/ingest/submission/state/SubmissionEnvelopeStateChangeListener.java
@@ -1,9 +1,11 @@
-package org.humancellatlas.ingest.submission;
+package org.humancellatlas.ingest.submission.state;
 
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.humancellatlas.ingest.core.MetadataDocument;
+import org.humancellatlas.ingest.submission.SubmissionEnvelope;
+import org.humancellatlas.ingest.submission.SubmissionEnvelopeRepository;
 import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
 import org.springframework.data.mongodb.core.mapping.event.AfterSaveEvent;
 import org.springframework.stereotype.Component;
