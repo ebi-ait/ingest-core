1930956a8074465086b184201ff8eacab6ba3617
Tony Burdett <tburdett@ebi.ac.uk>
2017-09-12 12:45:30 +0100

    adding mechanism on submission envelope to track and notify of state changes on contained docs

---
+++ src/main/java/org/humancellatlas/ingest/core/exception/MetadataDocumentStateException.java
@@ -0,0 +1,32 @@
+package org.humancellatlas.ingest.core.exception;
+
+/**
+ * Javadocs go here!
+ *
+ * @author Tony Burdett
+ * @date 12/09/17
+ */
+public class MetadataDocumentStateException extends RuntimeException {
+    public MetadataDocumentStateException() {
+        super();
+    }
+
+    public MetadataDocumentStateException(String message) {
+        super(message);
+    }
+
+    public MetadataDocumentStateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MetadataDocumentStateException(Throwable cause) {
+        super(cause);
+    }
+
+    protected MetadataDocumentStateException(String message,
+                                             Throwable cause,
+                                             boolean enableSuppression,
+                                             boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
--- src/main/java/org/humancellatlas/ingest/submission/SubmissionEnvelope.java
+++ src/main/java/org/humancellatlas/ingest/submission/SubmissionEnvelope.java
@@ -1,14 +1,25 @@
 package org.humancellatlas.ingest.submission;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
-import org.humancellatlas.ingest.core.*;
+import org.humancellatlas.ingest.core.AbstractEntity;
+import org.humancellatlas.ingest.core.EntityType;
+import org.humancellatlas.ingest.core.Event;
+import org.humancellatlas.ingest.core.MetadataDocument;
+import org.humancellatlas.ingest.core.SubmissionDate;
+import org.humancellatlas.ingest.core.UpdateDate;
+import org.humancellatlas.ingest.core.Uuid;
+import org.humancellatlas.ingest.core.ValidationState;
+import org.humancellatlas.ingest.core.exception.MetadataDocumentStateException;
 import org.humancellatlas.ingest.submission.state.InvalidSubmissionStateException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Javadocs go here!
@@ -19,6 +30,8 @@ import java.util.List;
 @Getter
 public class SubmissionEnvelope extends AbstractEntity {
     private final List<Event> events;
+
+    private final @JsonIgnore Map<String, ValidationState> validationStateMap;
 
     private SubmissionState submissionState;
 
@@ -34,6 +47,7 @@ public class SubmissionEnvelope extends AbstractEntity {
                               SubmissionState submissionState) {
         super(EntityType.SUBMISSION, uuid, submissionDate, updateDate);
         this.events = new ArrayList<>();
+        this.validationStateMap = new HashMap<>();
         this.submissionState = submissionState;
     }
 
@@ -94,10 +108,32 @@ public class SubmissionEnvelope extends AbstractEntity {
     public SubmissionEnvelope enactStateTransition(SubmissionState targetState) {
         if (!allowedStateTransitions().contains(targetState)) {
             throw new InvalidSubmissionStateException(String.format("The submission state '%s' is not recognised " +
-                    "as a submission envelope state that can be set", submissionState.name()));
+                                                                            "as a submission envelope state that can be set",
+                                                                    submissionState.name()));
         }
         this.submissionState = targetState;
 
         return this;
+    }
+
+    public SubmissionEnvelope notifyMetadataDocumentState(MetadataDocument metadataDocument) {
+        if (!isMetadataDocumentBeingTracked(metadataDocument)) {
+            // if it's pending it's a new document or has new content, so it's ok to add to state tracker
+            if (!metadataDocument.getValidationState().equals(ValidationState.PENDING)) {
+                this.validationStateMap.put(metadataDocument.getId(), metadataDocument.getValidationState());
+                throw new MetadataDocumentStateException(String.format(
+                        "Metadata document '%s' was not being tracked by containing envelope '%s' and does not have new content",
+                        metadataDocument,
+                        this));
+            }
+        }
+        this.validationStateMap.put(metadataDocument.getId(), metadataDocument.getValidationState());
+
+        return this;
+    }
+
+
+    public boolean isMetadataDocumentBeingTracked(MetadataDocument metadataDocument) {
+        return validationStateMap.containsKey(metadataDocument.getId());
     }
 }
