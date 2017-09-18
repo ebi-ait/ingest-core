4fa12a28b51266bc118b39fc15dd5389db2aa3cf
Rolando Fernandez <rolando@ebi.ac.uk>
2017-09-18 12:14:11 +0100

    reduced buffer wait time

--- src/main/java/org/humancellatlas/ingest/messaging/MessageSender.java
+++ src/main/java/org/humancellatlas/ingest/messaging/MessageSender.java
@@ -36,7 +36,7 @@ public class MessageSender {
         while(!messageQueue.isEmpty()){
             QueuedMessage nextMessage = messageQueue.peek();
             // get one minute ago
-            Date oneMinuteAgo = Date.from(Instant.now().minus(1, ChronoUnit.MINUTES));
+            Date oneMinuteAgo = Date.from(Instant.now().minus(10, ChronoUnit.SECONDS));
             if (nextMessage.getQueuedDate().before(oneMinuteAgo)) {
                 QueuedMessage message = messageQueue.remove();
                 this.rabbitMessagingTemplate.convertAndSend(message.getExchange(), message.getRoutingKey(), message.getPayload());
