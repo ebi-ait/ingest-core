1cc678fa762abdd810741efb698645de05b09f90
Rolando Fernandez <rolando@ebi.ac.uk>
2017-11-03 22:51:55 +0000

    added support for new dcp media types

---
+++ src/main/java/org/humancellatlas/ingest/file/web/FileMediaTypes.java
@@ -0,0 +1,13 @@
+package org.humancellatlas.ingest.file.web;
+
+/**
+ * Created by rolando on 10/10/2017.
+ */
+public class FileMediaTypes {
+    public static final String HCA_DATA_FILE = "data";
+    public static final String HCA_SAMPLE = "metadata/sample";
+    public static final String HCA_PROJECT = "metadata/project";
+    public static final String HCA_PROTOCOL = "metadata/protocol";
+    public static final String HCA_ASSAY = "metadata/assay";
+    public static final String HCA_ANALYSIS = "metadata/analysis";
+}
--- src/main/java/org/humancellatlas/ingest/file/web/FileMessage.java
+++ src/main/java/org/humancellatlas/ingest/file/web/FileMessage.java
@@ -1,9 +1,12 @@
 package org.humancellatlas.ingest.file.web;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.apache.http.entity.ContentType;
 import org.humancellatlas.ingest.core.Checksums;
+import org.springframework.util.StringUtils;
 
 /**
  * Created by rolando on 07/09/2017.
@@ -22,4 +25,13 @@ public class FileMessage {
     private final Checksums checksums;
     private final long size;
 
+    /**
+     * given existence of substring "dcp-type={type}" in this.contentType, extracts {type}
+     *
+     * @return the DCP media-type of the file uploaded that triggered this event
+     */
+    @JsonIgnore
+    public String getMediaType(){
+        return ContentType.parse(this.getContentType()).getParameter("dcp-type");
+    }
 }
