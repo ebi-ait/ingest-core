ca2f7230920346af8855490016c0c00975d91aa9
Daniel Vaughan <dan@danielvaughan.com>
2018-03-11 09:22:24 +0000

    Added v5 examples

--- src/main/java/org/humancellatlas/ingest/IngestCoreApplication.java
+++ src/main/java/org/humancellatlas/ingest/IngestCoreApplication.java
@@ -2,13 +2,12 @@ package org.humancellatlas.ingest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
-@EnableMongoAuditing(auditorAwareRef = "userAudtiting")
+@EnableMongoAuditing(auditorAwareRef = "userAuditing")
 @PropertySources({
         @PropertySource("classpath:application.properties"),
         @PropertySource("classpath:auth0.properties")
