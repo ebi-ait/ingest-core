8f1bb2e5161540c052152c9560db4da2dc15e177
Daniel Vaughan <dvaughan@ebi.ac.uk>
2018-03-12 11:26:05 +0000

    Added relationships: - Biomaterial to Project - Process to Project - Process to Protocol

--- src/main/java/org/humancellatlas/ingest/process/Process.java
+++ src/main/java/org/humancellatlas/ingest/process/Process.java
@@ -1,16 +1,19 @@
 package org.humancellatlas.ingest.process;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.Getter;
 import org.humancellatlas.ingest.biomaterial.Biomaterial;
+import org.humancellatlas.ingest.bundle.BundleManifest;
 import org.humancellatlas.ingest.core.EntityType;
 import org.humancellatlas.ingest.core.MetadataDocument;
 import org.humancellatlas.ingest.file.File;
+import org.humancellatlas.ingest.project.Project;
 import org.humancellatlas.ingest.protocol.Protocol;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by rolando on 16/02/2018.
@@ -20,6 +23,9 @@ public class Process extends MetadataDocument {
 
   @RestResource @DBRef private final List<Biomaterial> inputBiomaterials = new ArrayList<>();
   @RestResource @DBRef private final List<File> inputFiles = new ArrayList<>();
+  @RestResource @DBRef private final List<Project> projects = new ArrayList<>();
+  @RestResource @DBRef private final List<Protocol> protocols = new ArrayList<>();
+  @RestResource @DBRef private final List<BundleManifest> inputBundleManifests = new ArrayList<>();
 
   @DBRef private final Process inputProcess = null;
   @DBRef private final Protocol protocol = null;
