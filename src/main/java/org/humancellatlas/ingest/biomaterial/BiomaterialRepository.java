a51884994b3e13a109ccf7fa3e8d2f118daf802a
Rolando Fernandez <rolando@ebi.ac.uk>
2018-03-19 17:34:49 +0000

    querying by validation state

--- src/main/java/org/humancellatlas/ingest/biomaterial/BiomaterialRepository.java
+++ src/main/java/org/humancellatlas/ingest/biomaterial/BiomaterialRepository.java
@@ -6,6 +6,7 @@ import org.humancellatlas.ingest.submission.SubmissionEnvelope;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -20,7 +21,7 @@ public interface BiomaterialRepository extends MongoRepository<Biomaterial, Stri
 
   Page<Biomaterial> findBySubmissionEnvelopesContaining(SubmissionEnvelope submissionEnvelope, Pageable pageable);
 
-  Page<Biomaterial> findBySubmissionEnvelopesContainingAndValidationState(SubmissionEnvelope submissionEnvelope, ValidationState state, Pageable pageable);
+  Page<Biomaterial> findBySubmissionEnvelopesContainingAndValidationState(@Param("envelopeId") SubmissionEnvelope submissionEnvelope, @Param("validationState") ValidationState state, Pageable pageable);
 
   @RestResource(exported = false)
   List<Biomaterial> findByInputToProcessesContains(Process process);
