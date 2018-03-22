7fb2d4f88128cac2016601edf0bbb8c89a6b5191
Tony Burdett <tburdett@ebi.ac.uk>
2018-03-22 11:24:43 +0000

    updating biomaterial model object, methods to add input and output processes

--- src/main/java/org/humancellatlas/ingest/biomaterial/Biomaterial.java
+++ src/main/java/org/humancellatlas/ingest/biomaterial/Biomaterial.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Getter;
 import org.humancellatlas.ingest.core.EntityType;
 import org.humancellatlas.ingest.core.MetadataDocument;
+import org.humancellatlas.ingest.file.File;
 import org.humancellatlas.ingest.process.Process;
 import org.humancellatlas.ingest.project.Project;
 import org.springframework.data.mongodb.core.mapping.DBRef;
@@ -30,4 +31,29 @@ public class Biomaterial extends MetadataDocument {
     public Biomaterial(Object content) {
         super(EntityType.BIOMATERIAL, content);
     }
+
+    /**
+     * Adds to the collection of processes that this biomaterial serves as an input to
+     *
+     * @param process the process to add
+     * @return a reference to this biomaterial
+     */
+    public Biomaterial addAsInputToProcess(Process process) {
+        this.inputToProcesses.add(process);
+
+        return this;
+    }
+
+    /**
+     * Adds to the collection of processes that this biomaterial was derived by
+     *
+     * @param process the process to add
+     * @return a reference to this biomaterial
+     */
+    public Biomaterial addAsDerivedByProcess(Process process) {
+        this.derivedByProcesses.add(process);
+
+        return this;
+    }
+
 }
