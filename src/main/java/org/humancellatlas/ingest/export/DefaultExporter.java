f6f9311a8f49e898951b2e31c3632ba7e9439ff6
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-04-06 10:31:15 +0100

    Marked DefaultExporter as Spring component.

--- src/main/java/org/humancellatlas/ingest/export/DefaultExporter.java
+++ src/main/java/org/humancellatlas/ingest/export/DefaultExporter.java
@@ -6,6 +6,7 @@ import org.humancellatlas.ingest.process.Process;
 import org.humancellatlas.ingest.process.ProcessService;
 import org.humancellatlas.ingest.submission.SubmissionEnvelope;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+@Component
 public class DefaultExporter implements Exporter {
 
     @Autowired
