355c919be81bd3c18f77c84d7076d3797d7292cd
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-04-06 16:23:54 +0100

    Added check for queued message data.

--- src/test/java/org/humancellatlas/ingest/messaging/MessageSenderTest.java
+++ src/test/java/org/humancellatlas/ingest/messaging/MessageSenderTest.java
@@ -3,6 +3,9 @@ package org.humancellatlas.ingest.messaging;
 import org.humancellatlas.ingest.messaging.model.AssaySubmittedMessage;
 import org.junit.Test;
 
+import java.util.Date;
+import java.util.Queue;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MessageSenderTest {
@@ -16,10 +19,22 @@ public class MessageSenderTest {
         AssaySubmittedMessage message = new AssaySubmittedMessage("", "", "", "", "", "", 0, 0);
 
         //when:
+        Date timestamp = new Date();
         sender.queueNewAssayMessage("queue.exchange", "queue.route", message);
 
         //then:
-        assertThat(sender.getAssayMessageBatch()).hasSize(1);
+        Queue<MessageSender.QueuedMessage> queue = sender.getAssayMessageBatch();
+        assertThat(queue).hasSize(1);
+
+        //and:
+        MessageSender.QueuedMessage queuedMessage = queue.peek();
+        assertThat(queuedMessage)
+                .extracting("exchange", "routingKey", "payload")
+                .containsExactly("queue.exchange", "queue.route", message);
+
+        //and:
+        int _500MilliSeconds = 500;
+        assertThat(queuedMessage.getQueuedDate()).isCloseTo(timestamp, _500MilliSeconds);
     }
 
 }
