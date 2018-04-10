c635e7e5b95be65347961d582588f87c5b8aff39
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-04-10 16:38:27 +0100

    Refactored callback link generator code.

--- src/main/java/org/humancellatlas/ingest/core/web/SpringLinkGenerator.java
+++ src/main/java/org/humancellatlas/ingest/core/web/SpringLinkGenerator.java
@@ -13,20 +13,13 @@ import java.net.URI;
 @Component
 public class SpringLinkGenerator implements LinkGenerator {
 
-    private static final String DUMMY_BASE_URI = "http://localhost:8080";
-
     @Autowired
     private ResourceMappings resourceMappings;
 
     @Override
     public String createCallback(Class<?> documentType, String documentId) {
-        /* todo - here, we make link with DUMMY_BASE_URI and then take it out again so clients
-           can fill in domain - must be a better way of doing this! */
         ResourceMetadata metadata = resourceMappings.getMetadataFor(documentType);
-        RepositoryLinkBuilder linkBuilder = new RepositoryLinkBuilder(metadata,
-                new BaseUri(URI.create(DUMMY_BASE_URI)));
-        Link link = linkBuilder.slash(documentId).withRel(metadata.getItemResourceRel());
-        return link.withSelfRel().getHref().replace(DUMMY_BASE_URI, "");
+        return String.format("/%s/%s", metadata.getRel(), documentId);
     }
 
 }
