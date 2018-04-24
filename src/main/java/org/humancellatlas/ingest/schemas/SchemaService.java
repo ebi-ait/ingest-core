b93383fd89c4fd5ae811dc06c86570c3baa3a8fd
Rolando Fernandez <rolando@ebi.ac.uk>
2018-04-24 14:42:38 +0100

    tests for getting latest schemas

--- src/main/java/org/humancellatlas/ingest/schemas/SchemaService.java
+++ src/main/java/org/humancellatlas/ingest/schemas/SchemaService.java
@@ -41,23 +41,26 @@ public class SchemaService {
                                                                                                                                             pageable);
     }
 
-    public Collection<Schema> getLatestSchemas() {
+    public List<Schema> getLatestSchemas() {
         // a set initialized with a Comparator that deems two schemas to be equal if their concrete,domain,high-level
         // and sub-domain entities all match
-        Set<Schema> latestSchemas = new TreeSet<>((sch1,sch2) -> {
-           if(
-                   sch1.getConcreteEntity().equals(sch2.getConcreteEntity()) &&
-                   sch1.getDomainEntity().equals(sch2.getDomainEntity()) &&
-                   sch1.getHighLevelEntity().equals(sch2.getHighLevelEntity()) &&
-                   sch1.getSubDomainEntity().equals(sch2.getSubDomainEntity())) {
-               return 0;
-           } else {
-               return 1;
-           }
-        });
+        Set<Schema> latestSchemas = new TreeSet<>(this.latestSchemaComparator());
 
         schemaRepository.findAllByOrderBySchemaVersionDesc().forEach(latestSchemas::add);
-        return latestSchemas;
+        return new ArrayList<>(latestSchemas);
+    }
+
+    private Comparator<Schema> latestSchemaComparator() {
+        return (sch1, sch2) -> {
+            if(sch1.getConcreteEntity().equals(sch2.getConcreteEntity()) &&
+                    sch1.getDomainEntity().equals(sch2.getDomainEntity()) &&
+                    sch1.getHighLevelEntity().equals(sch2.getHighLevelEntity()) &&
+                    sch1.getSubDomainEntity().equals(sch2.getSubDomainEntity())) {
+                return 0;
+            } else {
+                return 1;
+            }
+        };
     }
 
     @Scheduled(fixedDelay = 1000 * 60 * 60 * 24) // ever 24 hours
