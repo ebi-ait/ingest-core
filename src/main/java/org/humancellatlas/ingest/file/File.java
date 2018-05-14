d5ff854230f359db6f7efa9b29010f9a8557795a
Alegria Aclan <aaclan@ebi.ac.uk>
2018-05-14 14:57:42 +0100

    Revert to old json creator behavior for file metadata

--- src/main/java/org/humancellatlas/ingest/file/File.java
+++ src/main/java/org/humancellatlas/ingest/file/File.java
@@ -28,8 +28,7 @@ public class File extends MetadataDocument {
     private Checksums checksums;
     private UUID validationId;
 
-    @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
-    public File(Object content) {
+    public File(@JsonProperty("content") Object content) {
         super(EntityType.FILE, content);
     }
 
