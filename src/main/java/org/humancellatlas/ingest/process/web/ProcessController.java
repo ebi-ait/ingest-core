c1b0455f5afd9d1b217c654fb021318d926f0a5e
Rolando Fernandez <rolando@ebi.ac.uk>
2018-06-29 11:23:37 +0100

    added controller method; search processes by input bundle uuid

--- src/main/java/org/humancellatlas/ingest/process/web/ProcessController.java
+++ src/main/java/org/humancellatlas/ingest/process/web/ProcessController.java
@@ -22,10 +22,9 @@ import org.springframework.hateoas.Resource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 /**
  * Created by rolando on 16/02/2018.
@@ -121,7 +120,13 @@ public class ProcessController {
         return ResponseEntity.accepted().body(resource);
     }
 
-
+    @RequestMapping(path = "/processes/search/findByInputBundleUuid", method = RequestMethod.GET)
+    ResponseEntity<?> findProcesessByInputBundleUuid(@RequestParam String bundleUuid,
+                                                     Pageable pageable,
+                                                     final PersistentEntityResourceAssembler resourceAssembler) {
+        Page<Process> processes = processService.processesByInputBundleUuid(UUID.fromString(bundleUuid), pageable);
+        return ResponseEntity.ok(pagedResourcesAssembler.toResource(processes, resourceAssembler));
+    }
 
     @RequestMapping(path = "/processes/{id}" + Links.VALIDATING_URL, method = RequestMethod.PUT)
     HttpEntity<?> validatingProcess(@PathVariable("id") Process process,
