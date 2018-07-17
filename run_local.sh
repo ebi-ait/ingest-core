d411935f25e1a33cd16b0e8480ea3162760c0364
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-07-17 11:39:11 +0100

    Defined shell script for running Ingest Core on host machine directly.

---
+++ run_local.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+docker-compose up -d mongo rabbitmq
+
+export MONGO_URI=mongodb://localhost:27017/admin
+export RABBIT_HOST=localhost
+export RABBIT_PORT=5672
+export SCHEMA_BASE_URI=https://schema.humancellatlas.org
+
+java \
+    -XX:+UseG1GC \
+    -jar build/libs/*.jar \
+    --spring.data.mongodb.uri=$MONGO_URI \
+    --spring.rabbitmq.host=$RABBIT_HOST \
+    --spring.rabbitmq.port=$RABBIT_PORT \
+    --schema.base-uri=$SCHEMA_BASE_URI
