631bbb1d82915c3eaac97c5fbb2743f9ecef7656
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-07-27 15:57:51 +0100

    Modified MessageSender to queue up messages the moment they're received.

--- src/main/java/org/humancellatlas/ingest/messaging/MessageSender.java
+++ src/main/java/org/humancellatlas/ingest/messaging/MessageSender.java
@@ -1,102 +1,47 @@
 package org.humancellatlas.ingest.messaging;
 
 import lombok.*;
-
+import org.humancellatlas.ingest.messaging.model.AbstractEntityMessage;
 import org.humancellatlas.ingest.messaging.model.ExportMessage;
 import org.humancellatlas.ingest.messaging.model.MetadataDocumentMessage;
-import org.humancellatlas.ingest.messaging.model.AbstractEntityMessage;
 import org.humancellatlas.ingest.messaging.model.SubmissionEnvelopeMessage;
 import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.*;
+import java.util.Date;
+import java.util.Objects;
 
 @Service
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class MessageSender {
 
     private @Autowired @NonNull RabbitMessagingTemplate rabbitMessagingTemplate;
 
-    private final @NonNull Queue<QueuedMessage> validationMessageBatch = new PriorityQueue<>(Comparator.comparing(QueuedMessage::getQueuedDate));
-    private final @NonNull Queue<QueuedMessage> accessionMessageBatch = new PriorityQueue<>(Comparator.comparing(QueuedMessage::getQueuedDate));
-    private final @NonNull Queue<QueuedMessage> exportMessageBatch = new PriorityQueue<>(Comparator.comparing(QueuedMessage::getQueuedDate));
-    private final @NonNull Queue<QueuedMessage> uploadManagerMessageBatch = new PriorityQueue<>(Comparator.comparing(QueuedMessage::getQueuedDate));
-    private final @NonNull Queue<QueuedMessage> stateTrackingMessageBatch = new PriorityQueue<>(Comparator.comparing(QueuedMessage::getQueuedDate));
-
-    private final int DELAY_TIME_VALIDATION_MESSAGES = 3;
-    private final int DELAY_TIME_NEW_EXPORT_MESSAGES = 5;
-    private final int DELAY_TIME_UPLOAD_MANAGER_MESSAGES = 1;
-    private final int DELAY_TIME_ACCESSIONER_MESSAGES = 2;
-    private final int DELAY_TIME_STATE_TRACKING_MESSAGES = 0;
-
-
-    public void queueValidationMessage(String exchange, String routingKey, MetadataDocumentMessage payload){
-        QueuedMessage message = new QueuedMessage(new Date(), exchange, routingKey, payload);
-        this.validationMessageBatch.add(message);
+    public void queueValidationMessage(String exchange, String routingKey,
+            MetadataDocumentMessage payload) {
+        rabbitMessagingTemplate.convertAndSend(exchange, routingKey, payload);
     }
 
-    public void queueAccessionMessage(String exchange, String routingKey, MetadataDocumentMessage payload){
-        QueuedMessage message = new QueuedMessage(new Date(), exchange, routingKey, payload);
-        this.accessionMessageBatch.add(message);
+    public void queueAccessionMessage(String exchange, String routingKey, MetadataDocumentMessage
+            payload) {
+        rabbitMessagingTemplate.convertAndSend(exchange, routingKey, payload);
     }
 
-    public void queueNewExportMessage(String exchange, String routingKey, ExportMessage payload){
-        QueuedMessage message = new QueuedMessage(new Date(), exchange, routingKey, payload);
-        this.exportMessageBatch.add(message);
+    public void queueNewExportMessage(String exchange, String routingKey, ExportMessage payload) {
+        rabbitMessagingTemplate.convertAndSend(exchange, routingKey, payload);
     }
 
-    public void queueStateTrackingMessage(String exchange, String routingKey, AbstractEntityMessage payload){
-        QueuedMessage message = new QueuedMessage(new Date(), exchange, routingKey, payload);
-        this.stateTrackingMessageBatch.add(message);
+    public void queueStateTrackingMessage(String exchange, String routingKey,
+            AbstractEntityMessage payload) {
+        rabbitMessagingTemplate.convertAndSend(exchange, routingKey, payload);
     }
 
-    public void queueUploadManagerMessage(String exchange, String routingKey, SubmissionEnvelopeMessage payload) {
-        QueuedMessage message = new QueuedMessage(new Date(), exchange, routingKey, payload);
-        this.uploadManagerMessageBatch.add(message);
-    }
-
-
-    @Scheduled(fixedDelay = 1000)
-    private void sendValidationMessages(){
-        sendFromQueue(this.validationMessageBatch, this.DELAY_TIME_VALIDATION_MESSAGES);
-    }
-
-    @Scheduled(fixedDelay = 1000)
-    private void sendAccessionMessages(){
-        sendFromQueue(this.accessionMessageBatch, this.DELAY_TIME_ACCESSIONER_MESSAGES);
-    }
-
-    @Scheduled(fixedDelay = 1000)
-    private void sendExportMessages(){
-        sendFromQueue(this.exportMessageBatch, this.DELAY_TIME_NEW_EXPORT_MESSAGES);
-    }
-
-    @Scheduled(fixedDelay = 1000)
-    private void sendStateTrackerMessages(){
-        sendFromQueue(this.stateTrackingMessageBatch, this.DELAY_TIME_STATE_TRACKING_MESSAGES);
-    }
-
-    @Scheduled(fixedDelay = 1000)
-    private void sendUploadManagerMessages(){
-        sendFromQueue(this.uploadManagerMessageBatch, this.DELAY_TIME_UPLOAD_MANAGER_MESSAGES);
-    }
-
-    private void sendFromQueue(Queue<QueuedMessage> messageQueue, int delayTimeSeconds){
-        while(!messageQueue.isEmpty()){
-            QueuedMessage nextMessage = messageQueue.peek();
-            Date messageWaitTime = Date.from(Instant.now().minus(delayTimeSeconds, ChronoUnit.SECONDS));
-            if (nextMessage.getQueuedDate().before(messageWaitTime)) {
-                QueuedMessage message = messageQueue.remove();
-                this.rabbitMessagingTemplate.convertAndSend(message.getExchange(), message.getRoutingKey(), message.getPayload());
-            } else {
-                break;
-            }
-        }
+    public void queueUploadManagerMessage(String exchange, String routingKey,
+            SubmissionEnvelopeMessage payload) {
+        rabbitMessagingTemplate.convertAndSend(exchange, routingKey, payload);
     }
 
     @Data
--- src/test/java/org/humancellatlas/ingest/messaging/MessageSenderTest.java
+++ src/test/java/org/humancellatlas/ingest/messaging/MessageSenderTest.java
@@ -2,39 +2,35 @@ package org.humancellatlas.ingest.messaging;
 
 import org.humancellatlas.ingest.messaging.model.ExportMessage;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
 
 import java.util.Date;
 import java.util.Queue;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class MessageSenderTest {
 
     @Test
     public void testQueueNewAssayMessage() {
         //given:
-        MessageSender sender = new MessageSender();
+        RabbitMessagingTemplate messageQueueTemplate = mock(RabbitMessagingTemplate.class);
+        MessageSender sender = new MessageSender(messageQueueTemplate);
 
         //and:
         ExportMessage message = new ExportMessage("", "", "", "", "", "", 0, 0);
 
         //when:
         Date timestamp = new Date();
-        sender.queueNewExportMessage("queue.exchange", "queue.route", message);
+        String exchange = "queue.exchange";
+        String routingKey = "queue.route";
+        sender.queueNewExportMessage(exchange, routingKey, message);
 
         //then:
-        Queue<MessageSender.QueuedMessage> queue = sender.getExportMessageBatch();
-        assertThat(queue).hasSize(1);
-
-        //and:
-        MessageSender.QueuedMessage queuedMessage = queue.peek();
-        assertThat(queuedMessage)
-                .extracting("exchange", "routingKey", "payload")
-                .containsExactly("queue.exchange", "queue.route", message);
-
-        //and:
-        int _500MilliSeconds = 500;
-        assertThat(queuedMessage.getQueuedDate()).isCloseTo(timestamp, _500MilliSeconds);
+        verify(messageQueueTemplate).convertAndSend(exchange, routingKey, message);
     }
 
 }
