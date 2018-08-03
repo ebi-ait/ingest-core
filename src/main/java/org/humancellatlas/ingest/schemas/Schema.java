0c497d6a65fa94f2159a11207adb032d2b91810c
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-08-03 16:18:37 +0100

    Defined code to parse and compare semantic schema version.

--- src/main/java/org/humancellatlas/ingest/schemas/Schema.java
+++ src/main/java/org/humancellatlas/ingest/schemas/Schema.java
@@ -9,6 +9,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.humancellatlas.ingest.core.AbstractEntity;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @AllArgsConstructor
 @Getter
 public class Schema extends AbstractEntity implements Comparable<Schema> {
@@ -23,8 +26,59 @@ public class Schema extends AbstractEntity implements Comparable<Schema> {
     private final String schemaUri;
 
     @Override
-    public int compareTo(Schema o) {
-        return 0;
+    public int compareTo(Schema other) {
+        SemanticVersion otherSchemaVersion = SemanticVersion.parse(other.schemaVersion);
+        return SemanticVersion.parse(schemaVersion).compareTo(otherSchemaVersion);
+    }
+
+    private static class SemanticVersion implements Comparable<SemanticVersion> {
+
+        private static final Pattern VERSION_PATTERN = Pattern.compile("(?<major>\\p{Digit}+)" +
+                "(.(?<minor>\\p{Digit}+))??(.(?<patch>\\p{Digit}+))??");
+
+        final int major;
+        final int minor;
+        int patch;
+
+        SemanticVersion(int major, int minor, int patch) {
+            this.major = major;
+            this.minor = minor;
+            this.patch = patch;
+        }
+
+        static SemanticVersion parse(String version) {
+            Matcher match = VERSION_PATTERN.matcher(version);
+            if (match.matches()) {
+                int major = parseVersionSegment(match, "major");
+                int minor = parseVersionSegment(match, "minor");
+                int patch = parseVersionSegment(match, "patch");
+                return new SemanticVersion(major, minor, patch);
+            } else {
+                throw new RuntimeException("Invalid version format.");
+            }
+        }
+
+        private static int parseVersionSegment(Matcher match, String versionSegment) {
+            String segmentValue = match.group(versionSegment);
+            int value = 0;
+            if (segmentValue != null) {
+                value = Integer.parseInt(segmentValue);
+            }
+            return value;
+        }
+
+        @Override
+        public int compareTo(SemanticVersion other) {
+            int difference = major - other.major;
+            if (difference == 0) {
+                difference = minor - other.minor;
+                if (difference == 0) {
+                    difference = patch - other.patch;
+                }
+            }
+            return difference;
+        }
+
     }
 
 }
--- src/test/java/org/humancellatlas/ingest/schemas/SchemaTest.java
+++ src/test/java/org/humancellatlas/ingest/schemas/SchemaTest.java
@@ -10,11 +10,29 @@ public class SchemaTest {
     @Test
     public void testCompareSame() {
         //given:
-        Schema schema = new Schema("core", "7.3.1", "process", "", "process_core",
-                "http://schema.humancellatlas.org");
+        Schema schema = createTestSchema("7.3.1");
 
         //expect:
         assertThat(schema.compareTo(schema)).isEqualTo(0);
+    }
+
+    @Test
+    public void testCompareOlder() {
+        //given:
+        Schema schemaVersion10 = createTestSchema("10.9");
+        Schema schemaVersion7 = createTestSchema("7.3.1");
+        Schema schemaVersion6_7 = createTestSchema("6.7.3");
+        Schema schemaVersion6_3 = createTestSchema("6.3.11");
+
+        //expect:
+        assertThat(schemaVersion10.compareTo(schemaVersion7)).isGreaterThan(0);
+        assertThat(schemaVersion7.compareTo(schemaVersion6_7)).isGreaterThan(0);
+        assertThat(schemaVersion6_7.compareTo(schemaVersion6_3)).isGreaterThan(0);
+    }
+
+    private Schema createTestSchema(String schemaVersion) {
+        return new Schema("core", schemaVersion, "process", "", "process_core",
+                "http://schema.humancellatlas.org");
     }
 
 }
