9ebc4a2bb99e14c79d4086730462a70b9a883104
Rolando Fernandez <rolando@ebi.ac.uk>
2018-09-07 11:38:52 +0100

    added a validation state change listener for selectively publishing messages to the state tracker

--- src/main/java/org/humancellatlas/ingest/state/ValidationStateChangeListener.java
+++ src/main/java/org/humancellatlas/ingest/state/ValidationStateChangeListener.java
@@ -1,4 +1,20 @@
 package org.humancellatlas.ingest.state;
 
-public class ValidationStateChangeListener {
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.humancellatlas.ingest.core.MetadataDocument;
+import org.humancellatlas.ingest.core.ValidationEvent;
+import org.humancellatlas.ingest.messaging.MessageRouter;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ValidationStateChangeListener implements ApplicationListener<ValidationEvent> {
+    private final @NonNull MessageRouter messageRouter;
+
+    @Override
+    public void onApplicationEvent(ValidationEvent event) {
+        messageRouter.routeStateTrackingUpdateMessageFor((MetadataDocument) event.getSource());
+    }
 }
