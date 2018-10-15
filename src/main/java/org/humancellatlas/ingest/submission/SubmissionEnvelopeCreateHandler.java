25a96b266282128df94c7330dd2b99ae769b3302
Rolando Fernandez <rolando@ebi.ac.uk>
2018-10-15 15:38:40 +0100

    beforeCreate instead of afterCreate

--- src/main/java/org/humancellatlas/ingest/submission/SubmissionEnvelopeCreateHandler.java
+++ src/main/java/org/humancellatlas/ingest/submission/SubmissionEnvelopeCreateHandler.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
 import org.springframework.data.rest.core.annotation.HandleAfterCreate;
+import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.core.mapping.ResourceMappings;
@@ -30,9 +31,14 @@ public class SubmissionEnvelopeCreateHandler {
     private final @NonNull RepositoryRestConfiguration config;
     private final @NonNull Logger log = LoggerFactory.getLogger(getClass());
 
+    @HandleBeforeCreate
+    public boolean submissionEnvelopeBeforeCreate(SubmissionEnvelope submissionEnvelope) {
+        submissionEnvelope.setUuid(Uuid.newUuid());
+        return true;
+    }
+
     @HandleAfterCreate
     public boolean handleSubmissionEnvelopeCreation(SubmissionEnvelope submissionEnvelope) {
-        submissionEnvelope.setUuid(Uuid.newUuid());
         this.messageRouter.routeStateTrackingNewSubmissionEnvelope(submissionEnvelope);
         this.messageRouter.routeRequestUploadAreaCredentials(submissionEnvelope);
         log.info(String.format("Submission envelope with ID %s was created.", submissionEnvelope.getId()));
