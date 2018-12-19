2ce4b8afa6913ec01d148018b9b76d9d32f3c885
Alegria Aclan <aaclan@ebi.ac.uk>
2018-12-19 10:54:38 +0000

    Secure messaging endpoints

--- src/main/java/org/humancellatlas/ingest/security/SecurityConfig.java
+++ src/main/java/org/humancellatlas/ingest/security/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/user/**").authenticated()
                 .antMatchers(HttpMethod.POST, "/submissionEnvelopes").authenticated()
+                .antMatchers(HttpMethod.POST, "/messaging/**").authenticated()
                 .antMatchers(HttpMethod.POST, "/projects").authenticated()
                 .antMatchers(HttpMethod.POST, "/submissionEnvelopes/*/projects").authenticated()
                 .antMatchers(HttpMethod.GET, "/**").permitAll();
