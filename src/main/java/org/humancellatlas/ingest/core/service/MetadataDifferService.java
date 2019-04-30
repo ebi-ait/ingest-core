27b33bed24ae99ce4f63ef4a188973e9c4f553bf
Rolando Fernandez <rolando@ebi.ac.uk>
2019-04-30 11:37:08 +0100

    service for generating diffs and checking if any difference between two objects

--- src/main/java/org/humancellatlas/ingest/core/service/MetadataDifferService.java
+++ src/main/java/org/humancellatlas/ingest/core/service/MetadataDifferService.java
@@ -1,4 +1,29 @@
 package org.humancellatlas.ingest.core.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.flipkart.zjsonpatch.JsonDiff;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.humancellatlas.ingest.core.MetadataDocument;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class MetadataDifferService {
+
+    public boolean anyDifference(MetadataDocument source, MetadataDocument target) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        JsonNode sourceContent = objectMapper.valueToTree(source.getContent());
+        JsonNode targetContent = objectMapper.valueToTree(target.getContent());
+
+        return ! sourceContent.equals(targetContent);
+    }
+
+    public JsonNode generatePatch(JsonNode source, JsonNode target) {
+        return JsonDiff.asJson(source, target);
+    }
+
 }
