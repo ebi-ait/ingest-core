ce4d7bf5aeff4b4a1a17d078b7fc2c47a46ee678
Rolando Fernandez <rolando@ebi.ac.uk>
2019-04-30 11:56:12 +0100

    using the refactored metadata crud service in the generic ValidationStateChangeService

--- src/main/java/org/humancellatlas/ingest/core/service/ValidationStateChangeService.java
+++ src/main/java/org/humancellatlas/ingest/core/service/ValidationStateChangeService.java
@@ -12,18 +12,14 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class ValidationStateChangeService {
-    private final @NonNull BiomaterialCrudStrategy biomaterialCrudStrategy;
-    private final @NonNull ProcessCrudStrategy processCrudStrategy;
-    private final @NonNull ProtocolCrudStrategy protocolCrudStrategy;
-    private final @NonNull ProjectCrudStrategy projectCrudStrategy;
-    private final @NonNull FileCrudStrategy fileCrudStrategy;
+    private final @NonNull MetadataCrudService metadataCrudService;
 
     private final @NonNull ValidationStateEventPublisher validationStateEventPublisher;
 
     public MetadataDocument changeValidationState(String metadataType,
                                                               String metadataId,
                                                               ValidationState validationState) {
-        MetadataCrudStrategy crudStrategy = crudStrategyForMetadataType(metadataType);
+        MetadataCrudStrategy crudStrategy = metadataCrudService.crudStrategyForMetadataType(metadataType);
         MetadataDocument metadataDocument = crudStrategy.findMetadataDocument(metadataId);
         metadataDocument.setValidationState(validationState);
         metadataDocument = crudStrategy.saveMetadataDocument(metadataDocument);
@@ -33,22 +29,5 @@ public class ValidationStateChangeService {
         return metadataDocument;
     }
 
-
-    private MetadataCrudStrategy crudStrategyForMetadataType(String metadataType) {
-        switch (metadataType) {
-            case "biomaterials":
-                return biomaterialCrudStrategy;
-            case "processes":
-                return processCrudStrategy;
-            case "protocols":
-                return protocolCrudStrategy;
-            case "projects":
-                return projectCrudStrategy;
-            case "files":
-                return fileCrudStrategy;
-            default:
-                throw new RuntimeException(String.format("No such metadata type: %s", metadataType));
-        }
-    }
 
 }
