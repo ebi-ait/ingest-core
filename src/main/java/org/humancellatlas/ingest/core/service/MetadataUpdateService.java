5075d4ccae162935d3fa4c4f42b1074e91bfd643
Rolando Fernandez <rolando@ebi.ac.uk>
2019-04-30 16:31:06 +0100

    generic method for handling updateDocuments; discards updateDocuments if they contain no diff with original

--- src/main/java/org/humancellatlas/ingest/core/service/MetadataUpdateService.java
+++ src/main/java/org/humancellatlas/ingest/core/service/MetadataUpdateService.java
@@ -1,4 +1,33 @@
-package org.humancellatlas.ingest.core.service.strategy;
+package org.humancellatlas.ingest.core.service;
 
+import lombok.AllArgsConstructor;
+
+import lombok.NonNull;
+import org.humancellatlas.ingest.core.MetadataDocument;
+import org.humancellatlas.ingest.core.exception.RedundantUpdateException;
+import org.humancellatlas.ingest.core.service.strategy.MetadataCrudStrategy;
+import org.humancellatlas.ingest.submission.SubmissionEnvelope;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
 public class MetadataUpdateService {
+    private final @NonNull MetadataDifferService metadataDifferService;
+    private final @NonNull MetadataCrudService metadataCrudService;
+
+    public <T extends MetadataDocument> T doUpdate(T updateDocument, SubmissionEnvelope submissionEnvelope) {
+        String documentType = updateDocument.getType().toString();
+        MetadataCrudStrategy metadataCrudStrategy = metadataCrudService.crudStrategyForMetadataType(updateDocument.getType().toString());
+        T originalDocument = (T) metadataCrudStrategy.findOriginalByUuid(updateDocument.getUuid().getUuid().toString());
+
+        if(metadataDifferService.anyDifference(originalDocument, updateDocument)) {
+            return metadataCrudService.addToSubmissionEnvelopeAndSave(updateDocument, submissionEnvelope);
+        } else {
+            throw new RedundantUpdateException(String.format("Attempted to update %s document at %s with contents of %s but there is no diff",
+                                                             documentType,
+                                                             originalDocument.getId(),
+                                                             updateDocument.getId()));
+        }
+    }
+
 }
