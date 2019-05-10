50360c2ac579d54048ae07c2a743ed09c4dec815
Rodrey Goite <rodrey@ebi.ac.uk>
2019-05-10 16:03:01 +0100

    Added back Ant path matchers when checking requests from proxy.

--- src/main/java/org/humancellatlas/ingest/security/SecurityConfig.java
+++ src/main/java/org/humancellatlas/ingest/security/SecurityConfig.java
@@ -10,13 +10,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration
+        .WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 
 @EnableWebSecurity
 @Configuration
@@ -35,9 +44,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Value(value= "${GCP_PROJECT_WHITELIST:hca-dcp-production.iam.gserviceaccount.com,human-cell-atlas-travis-test.iam.gserviceaccount.com,broad-dsde-mint-dev.iam.gserviceaccount.com,broad-dsde-mint-test.iam.gserviceaccount.com,broad-dsde-mint-staging.iam.gserviceaccount.com}")
     private String projectWhitelist;
 
+    private static final List<AntPathRequestMatcher> SECURED_ANT_PATHS;
+    static {
+        List<AntPathRequestMatcher> antPathMatchers = new ArrayList<>();
+        antPathMatchers.addAll(defineAntPathMatchers(GET, "/user/**"));
+        antPathMatchers.addAll(defineAntPathMatchers(POST, "/submissionEnvelopes",
+                "/messaging/**", "/projects", "/submissionEnvelopes/*/projects"));
+        SECURED_ANT_PATHS = Collections.unmodifiableList(antPathMatchers);
+    }
+
+    private static List<AntPathRequestMatcher> defineAntPathMatchers(HttpMethod method,
+            String...patterns) {
+        return Stream.of(patterns)
+                .map(pattern -> new AntPathRequestMatcher(method.name(), pattern))
+                .collect(toList());
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        List<String> projectWhitelist = Arrays.asList(this.projectWhitelist.split(","));
+        List<String> projectWhitelist = asList(this.projectWhitelist.split(","));
         GoogleServiceJwtAuthenticationProvider googleServiceJwtAuthenticationProvider = new GoogleServiceJwtAuthenticationProvider(serviceAudience, projectWhitelist);
 
         JwkProvider jwkProvider = new JwkProviderBuilder(issuer).build();
@@ -63,12 +88,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .cors().and()
                 .authorizeRequests()
-                .requestMatchers(this::isRequestFromProxy).authenticated()
-                .antMatchers(HttpMethod.GET, "/**").permitAll();
+                .requestMatchers(this::isRequestForSecuredResourceFromProxy).authenticated()
+                .antMatchers(GET, "/**").permitAll();
     }
 
-    private Boolean isRequestFromProxy(HttpServletRequest request) {
-        return Optional.ofNullable(request.getHeader(FORWARDED_FOR)).isPresent();
+    private Boolean isRequestForSecuredResourceFromProxy(HttpServletRequest request) {
+        return SECURED_ANT_PATHS.stream().anyMatch(matcher -> matcher.matches(request)) &&
+                Optional.ofNullable(request.getHeader(FORWARDED_FOR)).isPresent();
     }
 
 }
