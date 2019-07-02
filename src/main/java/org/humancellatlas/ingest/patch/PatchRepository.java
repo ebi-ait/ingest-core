abd6c4850741bea08eed8c6d706fcb77d28e2937
Rodrey Goite <rodrey@ebi.ac.uk>
2019-07-02 15:32:51 +0100

    Defined repository method to retrieve Patch by update document id.

--- src/main/java/org/humancellatlas/ingest/patch/PatchRepository.java
+++ src/main/java/org/humancellatlas/ingest/patch/PatchRepository.java
@@ -1,9 +1,19 @@
 package org.humancellatlas.ingest.patch;
 
+import org.bson.types.ObjectId;
+import org.humancellatlas.ingest.core.MetadataDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
 
 @CrossOrigin
 public interface PatchRepository extends MongoRepository<Patch, String> {
+
+    @RestResource(path="updatedocument")
+    @Query("{ 'updateDocument.$id': ?0 }")
+    Patch<? extends MetadataDocument> findByUpdateDocumentId(ObjectId id);
 
 }
