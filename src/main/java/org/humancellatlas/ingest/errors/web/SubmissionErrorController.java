a0fec84eaca4e01ade805b4ac8b6e8d7dc0096b9
Alexie Staffer <astaffer@ebi.ac.uk>
2019-08-07 17:57:15 +0100

    Fixup addErrorToEnvelope POST: SubmissionError

--- src/main/java/org/humancellatlas/ingest/errors/web/SubmissionErrorController.java
+++ src/main/java/org/humancellatlas/ingest/errors/web/SubmissionErrorController.java
@@ -1,34 +1,30 @@
 package org.humancellatlas.ingest.errors.web;
 
+import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.humancellatlas.ingest.errors.SubmissionError;
-import org.humancellatlas.ingest.errors.SubmissionErrorService;
+import org.humancellatlas.ingest.errors.*;
 import org.humancellatlas.ingest.submission.SubmissionEnvelope;
-import org.springframework.data.rest.webmvc.PersistentEntityResource;
 import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.hateoas.ExposesResourceFor;
 import org.springframework.hateoas.Resource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @RepositoryRestController
 @ExposesResourceFor(SubmissionError.class)
 @RequiredArgsConstructor
+@Getter
 public class SubmissionErrorController {
     private final @NonNull SubmissionErrorService submissionErrorService;
 
-    @RequestMapping(path = "submissionEnvelopes/{sub_id}/submissionErrors", method = RequestMethod.POST)
+    @PostMapping(path = "submissionEnvelopes/{sub_id}/submissionErrors")
     ResponseEntity<Resource<?>> addErrorToEnvelope(@PathVariable("sub_id") SubmissionEnvelope submissionEnvelope,
                                                    @RequestBody SubmissionError submissionError,
-                                                   PersistentEntityResourceAssembler assembler) {
+                                                   final PersistentEntityResourceAssembler resourceAssembler) {
 
-        SubmissionEnvelope envelope = submissionErrorService.addErrorToEnvelope(submissionError, submissionEnvelope);
-        PersistentEntityResource resource = assembler.toFullResource(envelope);
-        return ResponseEntity.accepted().body(resource);
+        SubmissionEnvelope envelope = getSubmissionErrorService().addErrorToEnvelope(submissionError, submissionEnvelope);
+        return ResponseEntity.accepted().body(resourceAssembler.toFullResource(envelope));
     }
 }
