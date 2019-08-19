2b40d5e4e7c1db568dc9dd1eb3f1772e638f3299
prabh-t <prabhat.totoo@gmail.com>
2019-08-19 11:36:30 +0100

    Adding validation state and errors to validation job object

--- src/main/java/org/humancellatlas/ingest/file/ValidationJob.java
+++ src/main/java/org/humancellatlas/ingest/file/ValidationJob.java
@@ -1,8 +1,11 @@
 package org.humancellatlas.ingest.file;
 
 import lombok.Data;
-import org.humancellatlas.ingest.core.Checksums;
 
+import org.humancellatlas.ingest.core.Checksums;
+import org.humancellatlas.ingest.state.ValidationState;
+
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -10,6 +13,10 @@ public class ValidationJob {
     private UUID validationId;
     private Checksums checksums;
     private boolean jobCompleted;
+    private ValidationState validationState;
+    private List<Object> validationErrors;
+    
+    // TODO: maybe create new ValidationReport object with validationState, validationErrors: List<String>?
 
     protected ValidationJob() {}
 
