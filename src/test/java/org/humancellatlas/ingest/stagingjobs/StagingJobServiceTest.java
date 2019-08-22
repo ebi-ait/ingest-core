d2b3eaf3fd151da61422985d0b1358636fab6979
Rolando Fernandez <rolando@ebi.ac.uk>
2019-08-22 11:09:13 +0100

    testing happy path for registering new jobs

--- src/test/java/org/humancellatlas/ingest/stagingjobs/StagingJobServiceTest.java
+++ src/test/java/org/humancellatlas/ingest/stagingjobs/StagingJobServiceTest.java
@@ -7,19 +7,13 @@ import org.junit.Test;
 import static org.mockito.Mockito.*;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.dao.DuplicateKeyException;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 public class StagingJobServiceTest {
@@ -49,5 +43,35 @@ public class StagingJobServiceTest {
         stagingJobService.registerNewJob(testStagingAreaUuid, testFileName);
         assertThatExceptionOfType(IllegalStateException.class)
                 .isThrownBy(() -> stagingJobService.registerNewJob(testStagingAreaUuid, testFileName));
+    }
+
+    @Test
+    public void testRegisteringJobs() {
+        when(stagingJobRepository.save(any(StagingJob.class)))
+                .thenAnswer(new Answer<StagingJob>() {
+                    private Set<StagingJob> savedJobs = new HashSet<>();
+                    @Override
+                    public StagingJob answer(InvocationOnMock invocation){
+                        StagingJob stagingJob = invocation.getArgument(0);
+                        if(savedJobs.contains(stagingJob)) {
+                            throw new DuplicateKeyException("");
+                        } else {
+                            savedJobs.add(stagingJob);
+                            return stagingJob;
+                        }
+                    }
+                });
+
+        UUID testStagingAreaUuid_1 = UUID.randomUUID();
+        String testFileName_1 = "test_1.fastq.gz";
+
+        UUID testStagingAreaUuid_2 = UUID.randomUUID();
+        String testFileName_2 = "test_2.fastq.gz";
+
+        stagingJobService.registerNewJob(testStagingAreaUuid_1, testFileName_1);
+        stagingJobService.registerNewJob(testStagingAreaUuid_1, testFileName_2);
+
+        stagingJobService.registerNewJob(testStagingAreaUuid_2, testFileName_1);
+        stagingJobService.registerNewJob(testStagingAreaUuid_2, testFileName_2);
     }
 }
