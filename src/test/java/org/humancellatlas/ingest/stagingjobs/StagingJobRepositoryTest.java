ba3630a64a3468b93d10d7e7bc5649aea139bcd4
Rolando Fernandez <rolando@ebi.ac.uk>
2019-08-22 11:01:03 +0100

    testing happy path when adding non-conflicting jobs

--- src/test/java/org/humancellatlas/ingest/stagingjobs/StagingJobRepositoryTest.java
+++ src/test/java/org/humancellatlas/ingest/stagingjobs/StagingJobRepositoryTest.java
@@ -29,14 +29,29 @@ public class StagingJobRepositoryTest {
     StagingJobRepository stagingJobRepository;
 
     @Test
-    public void testJpaException() {
+    public void testJpaExceptionWhenInsertingMultipleCompoundKey() {
         UUID testStagingAreaUuid = UUID.randomUUID();
         String testFileName = "test.fastq.gz";
 
         stagingJobRepository.save(new StagingJob(testStagingAreaUuid, testFileName));
 
-        assertThatExceptionOfType(DuplicateKeyException.class).isThrownBy(()-> {
+        assertThatExceptionOfType(DuplicateKeyException.class).isThrownBy(() -> {
             stagingJobRepository.save(new StagingJob(testStagingAreaUuid, testFileName));
         });
+    }
+
+    @Test
+    public void testRegisteringJobsWithDifferentCompoundKey() {
+        UUID testStagingAreaUuid_1 = UUID.randomUUID();
+        String testFileName_1 = "test_1.fastq.gz";
+
+        UUID testStagingAreaUuid_2 = UUID.randomUUID();
+        String testFileName_2 = "test_2.fastq.gz";
+
+        stagingJobRepository.save(new StagingJob(testStagingAreaUuid_1, testFileName_1));
+        stagingJobRepository.save(new StagingJob(testStagingAreaUuid_1, testFileName_2));
+
+        stagingJobRepository.save(new StagingJob(testStagingAreaUuid_2, testFileName_1));
+        stagingJobRepository.save(new StagingJob(testStagingAreaUuid_2, testFileName_2));
     }
 }
