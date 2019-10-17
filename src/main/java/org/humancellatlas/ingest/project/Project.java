c90247b745bf0df13725ed450352a66144d8c405
prabht <prabhat@ebi.ac.uk>
2019-10-17 15:36:39 +0100

    reinstate methods to allow adding subEnv to project. moved from metadatadocument

--- src/main/java/org/humancellatlas/ingest/project/Project.java
+++ src/main/java/org/humancellatlas/ingest/project/Project.java
@@ -1,11 +1,14 @@
 package org.humancellatlas.ingest.project;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 import org.humancellatlas.ingest.core.EntityType;
 import org.humancellatlas.ingest.core.MetadataDocument;
+import org.humancellatlas.ingest.core.exception.LinkToNewSubmissionNotAllowedException;
 import org.humancellatlas.ingest.file.File;
+import org.humancellatlas.ingest.state.ValidationState;
 import org.humancellatlas.ingest.submission.SubmissionEnvelope;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.rest.core.annotation.RestResource;
@@ -30,5 +33,38 @@ public class Project extends MetadataDocument {
     @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
     public Project(Object content) {
         super(EntityType.PROJECT, content);
+    }
+
+    // A project may have 1 or more submissions.
+    private @DBRef(lazy = true) Set<SubmissionEnvelope> submissionEnvelopes = new HashSet<>();
+
+    // This method is moved from MetadataDocument to Project class during the subEnvs refactoring
+    // It makes sense to add subEnv to project, not all other entity types.
+    public MetadataDocument addToSubmissionEnvelope(SubmissionEnvelope submissionEnvelope) {
+        SubmissionEnvelope openSubmission = this.getOpenSubmissionEnvelope();
+        if( openSubmission == null ){
+            if(this.getValidationState() != ValidationState.DRAFT){
+                this.enactStateTransition(ValidationState.DRAFT);
+            }
+            this.submissionEnvelopes.add(submissionEnvelope);
+        }
+        else if (!openSubmission.getId().equals(submissionEnvelope.getId())){
+            String errorMessage = String.format("The %s metadata %s is still linked to a %s submission envelope %s.",
+                    this.getType(), this.getId(), openSubmission.getSubmissionState(), openSubmission.getId());
+            getLog().error(errorMessage);
+
+            throw new LinkToNewSubmissionNotAllowedException(errorMessage);
+        }
+        return this;
+    }
+
+    @JsonIgnore
+    public SubmissionEnvelope getOpenSubmissionEnvelope(){
+        for (SubmissionEnvelope submissionEnvelope : this.submissionEnvelopes) {
+            if (submissionEnvelope.isOpen()){
+                return submissionEnvelope;
+            }
+        }
+        return null;
     }
 }
