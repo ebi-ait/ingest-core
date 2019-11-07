7706e9e75496c140b465d4495d94716db903b9e8
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2019-11-07 12:38:50 +0000

    Migrate Submission Envelope Arrays to Singleton.

--- src/main/java/org/humancellatlas/ingest/migrations/MongoChangeLog.java
+++ src/main/java/org/humancellatlas/ingest/migrations/MongoChangeLog.java
@@ -5,6 +5,9 @@ import com.github.mongobee.changeset.ChangeSet;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @ChangeLog
 public class MongoChangeLog {
     @ChangeSet(order = "2019-10-30", id="featureCompatibilityVersion 3.4", author = "alexie.staffer@ebi.ac.uk")
@@ -26,5 +29,45 @@ public class MongoChangeLog {
     @ChangeSet(order = "2019-11-02", id="featureCompatibilityVersion 4.2", author = "alexie.staffer@ebi.ac.uk")
     public void featureCompatibilityFourTwo(MongoDatabase db) {
         db.runCommand( new Document("setFeatureCompatibilityVersion", "4.2") );
+    }
+
+    @ChangeSet(order = "2019-11-03", id="singletonSubmissionEnvelope Biomaterial", author = "alexie.staffer@ebi.ac.uk")
+    public void singletonSubmissionEnvelopeBiomaterial(MongoDatabase db) {
+        Document filter = new Document();
+        List<Document> update = new ArrayList<>();
+        update.add(new Document("$set", Document.parse("{ submissionEnvelope: { $arrayElemAt: [ \"$submissionEnvelopes\", 0 ] } }")));
+        update.add(new Document("$unset", "submissionEnvelopes"));
+
+        db.getCollection("biomaterial").updateMany(filter, update);
+    }
+
+    @ChangeSet(order = "2019-11-04", id="singletonSubmissionEnvelope Process", author = "alexie.staffer@ebi.ac.uk")
+    public void singletonSubmissionEnvelopeProcess(MongoDatabase db) {
+        Document filter = new Document();
+        List<Document> update = new ArrayList<>();
+        update.add(new Document("$set", Document.parse("{ submissionEnvelope: { $arrayElemAt: [ \"$submissionEnvelopes\", 0 ] } }")));
+        update.add(new Document("$unset", "submissionEnvelopes"));
+
+        db.getCollection("process").updateMany(filter, update);
+    }
+
+    @ChangeSet(order = "2019-11-05", id="singletonSubmissionEnvelope Protocol", author = "alexie.staffer@ebi.ac.uk")
+    public void singletonSubmissionEnvelopeProtocol(MongoDatabase db) {
+        Document filter = new Document();
+        List<Document> update = new ArrayList<>();
+        update.add(new Document("$set", Document.parse("{ submissionEnvelope: { $arrayElemAt: [ \"$submissionEnvelopes\", 0 ] } }")));
+        update.add(new Document("$unset", "submissionEnvelopes"));
+
+        db.getCollection("protocol").updateMany(filter, update);
+    }
+
+    @ChangeSet(order = "2019-11-06", id="singletonSubmissionEnvelope File", author = "alexie.staffer@ebi.ac.uk")
+    public void singletonSubmissionEnvelopeFile(MongoDatabase db) {
+        Document filter = new Document();
+        List<Document> update = new ArrayList<>();
+        update.add(new Document("$set", Document.parse("{ submissionEnvelope: { $arrayElemAt: [ \"$submissionEnvelopes\", 0 ] } }")));
+        update.add(new Document("$unset", "submissionEnvelopes"));
+
+        db.getCollection("file").updateMany(filter, update);
     }
 }
