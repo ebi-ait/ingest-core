f7fe49232bc2effd377982d962a61ecac1bac115
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2019-11-07 10:51:20 +0000

    Migrate feature compatibility to 4.0, disable monitoring advert.

--- src/main/java/org/humancellatlas/ingest/migrations/MongoChangeLog.java
+++ src/main/java/org/humancellatlas/ingest/migrations/MongoChangeLog.java
@@ -9,11 +9,17 @@ import org.bson.Document;
 public class MongoChangeLog {
     @ChangeSet(order = "2019-10-30", id="featureCompatibilityVersion 3.4", author = "alexie.staffer@ebi.ac.uk")
     public void featureCompatibilityThreeFour(MongoDatabase db) {
-        db.runCommand( new Document("setFeatureCompatibilityVersion", "3.4"));
+        db.runCommand( new Document("setFeatureCompatibilityVersion", "3.4") );
     }
 
     @ChangeSet(order = "2019-10-31", id="featureCompatibilityVersion 3.6", author = "alexie.staffer@ebi.ac.uk")
     public void featureCompatibilityThreeSix(MongoDatabase db) {
-        db.runCommand( new Document("setFeatureCompatibilityVersion", "3.6"));
+        db.runCommand( new Document("setFeatureCompatibilityVersion", "3.6") );
+    }
+
+    @ChangeSet(order = "2019-11-01", id="featureCompatibilityVersion 4.0", author = "alexie.staffer@ebi.ac.uk")
+    public void featureCompatibilityFourZero(MongoDatabase db) {
+        db.runCommand( new Document("setFeatureCompatibilityVersion", "4.0") );
+        db.runCommand( new Document("setFreeMonitoring", 1).append("action", "disable") );
     }
 }
