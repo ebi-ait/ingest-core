94a5b995794b0cdff1b3184949809e7fa6271906
Rodrey Goite <rodrey@ebi.ac.uk>
2019-11-14 10:02:44 +0000

    Added Javadoc description of RemoteJwkProvider.

--- src/main/java/org/humancellatlas/ingest/security/RemoteJwkProvider.java
+++ src/main/java/org/humancellatlas/ingest/security/RemoteJwkProvider.java
@@ -4,6 +4,9 @@ import com.auth0.jwk.UrlJwkProvider;
 
 import java.net.URL;
 
+/**
+ * A UrlJwkProvider that allows inspection of its internal URL.
+ */
 public class RemoteJwkProvider extends UrlJwkProvider {
 
     private final URL url;
