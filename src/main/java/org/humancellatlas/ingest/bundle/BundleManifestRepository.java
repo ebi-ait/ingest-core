63a5a38f0efa200e27f6bda5a41439aae03ea589
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2020-01-22 14:27:11 +0000

    Do not export internal BundleManifest.findByEnvelopeUuid

--- src/main/java/org/humancellatlas/ingest/bundle/BundleManifestRepository.java
+++ src/main/java/org/humancellatlas/ingest/bundle/BundleManifestRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.Optional;
@@ -24,5 +25,6 @@ public interface BundleManifestRepository extends MongoRepository<BundleManifest
 
     Long deleteByEnvelopeUuid (String uuid);
 
+    @RestResource(exported = false)
     Stream<BundleManifest> findByEnvelopeUuid(String envelopeUuid);
 }
