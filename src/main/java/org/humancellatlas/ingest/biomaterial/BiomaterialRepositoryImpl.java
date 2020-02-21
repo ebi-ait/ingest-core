c1dcee985c1a5f7140b80f81c7f8828f6e21fd94
Alegria Aclan <aaclan@ebi.ac.uk>
2020-02-21 16:22:49 +0000

    Added querying of biomaterials by its content

--- src/main/java/org/humancellatlas/ingest/biomaterial/BiomaterialRepositoryImpl.java
+++ src/main/java/org/humancellatlas/ingest/biomaterial/BiomaterialRepositoryImpl.java
@@ -1,4 +1,32 @@
 package org.humancellatlas.ingest.biomaterial;
 
-public class BiomaterialRepositoryImpl {
+import org.humancellatlas.ingest.query.MetadataCriteria;
+import org.humancellatlas.ingest.query.QueryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
+
+public class BiomaterialRepositoryImpl implements BiomaterialRepositoryCustom {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Override
+    public Page<Biomaterial> findByCriteria(List<MetadataCriteria> criteriaList, Boolean andCriteria, Pageable pageable) {
+        QueryBuilder queryBuilder = new QueryBuilder();
+        Query query = queryBuilder.build(criteriaList, andCriteria);
+
+        long count = mongoTemplate.count(query, Biomaterial.class);
+        query.with(pageable);
+        List<Biomaterial> result = mongoTemplate.find(query, Biomaterial.class);
+
+        return new PageImpl<>(result, pageable, count);
+    }
+
+
 }
