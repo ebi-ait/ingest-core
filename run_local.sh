2d1a598d7878b12ff0042f9a21fd20f9185c069c
Justin Clark-Casey <justincc@justincc.org>
2020-06-01 16:37:45 +0100

    add env vars from Dockerfile to run_local.sh

--- run_local.sh
+++ run_local.sh
@@ -7,6 +7,13 @@ export RABBIT_HOST=localhost
 export RABBIT_PORT=5672
 export SCHEMA_BASE_URI=https://schema.humancellatlas.org
 
+export AUTH_ISSUER=https://login.elixir-czech.org/oidc
+export SVC_AUTH_AUDIENCE=https://dev.data.humancellatlas.org/
+export USR_AUTH_AUDIENCE=https://dev.data.humancellatlas.org/
+export GCP_JWK_PROVIDER_BASE_URL=https://www.googleapis.com/service_accounts/v1/jwk/
+export GCP_PROJECT_WHITELIST=hca-dcp-production.iam.gserviceaccount.com,human-cell-atlas-travis-test.iam.gserviceaccount.com,broad-dsde-mint-dev.iam.gserviceaccount.com,broad-dsde-mint-test.iam.gserviceaccount.com,broad-dsde-mint-staging.iam.gserviceaccount.com
+export SCHEMA_BASE_URI=https://schema.dev.data.humancellatlas.org/
+
 ./gradlew clean assemble
 
 java \
@@ -16,3 +23,4 @@ java \
     --spring.rabbitmq.host=$RABBIT_HOST \
     --spring.rabbitmq.port=$RABBIT_PORT \
     --schema.base-uri=$SCHEMA_BASE_URI
+
