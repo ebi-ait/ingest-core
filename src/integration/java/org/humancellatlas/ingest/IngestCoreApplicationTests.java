75072478226db150a3f8186df01f1a04f73baaed
Rodrey Goite <rodrey@ebi.ac.uk>
2020-06-17 14:44:10 +0100

    Removed redundant annotation.

--- src/integration/java/org/humancellatlas/ingest/IngestCoreApplicationTests.java
+++ src/integration/java/org/humancellatlas/ingest/IngestCoreApplicationTests.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class IngestCoreApplicationTests {
 	@MockBean
