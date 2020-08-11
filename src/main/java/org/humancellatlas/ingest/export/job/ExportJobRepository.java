2a57db6e57e7640e786bcbd1d50fcab3a6615842
Rolando Fernandez <rolando@ebi.ac.uk>
2020-08-11 15:48:47 +0100

    added a findBySubmission search endpoint for ExportJobs

--- src/main/java/org/humancellatlas/ingest/export/job/ExportJobRepository.java
+++ src/main/java/org/humancellatlas/ingest/export/job/ExportJobRepository.java
@@ -3,14 +3,13 @@ package org.humancellatlas.ingest.export.job;
 import org.humancellatlas.ingest.export.ExportState;
 import org.humancellatlas.ingest.export.destination.ExportDestinationName;
 import org.humancellatlas.ingest.submission.SubmissionEnvelope;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin
 public interface ExportJobRepository extends MongoRepository<ExportJob, String> {
-    @RestResource(exported = false)
-    <T extends ExportJob> T findBySubmissionAndStatusAndDestinationNameAndDestinationVersion(
-        SubmissionEnvelope submissionEnvelope, ExportState exportState,
-        ExportDestinationName destinationName, String destinationVersion);
+    Page<ExportJob> findBySubmission(SubmissionEnvelope submissionEnvelope, Pageable pageable);
 }
