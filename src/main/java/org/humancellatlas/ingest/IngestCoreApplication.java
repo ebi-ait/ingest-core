97d445f3385d68cb8eea62df46be22e7b08c7a5e
prabh-t <prabhat@ebi.ac.uk>
2020-09-30 13:38:25 +0100

    Enabled mongock in core

--- src/main/java/org/humancellatlas/ingest/IngestCoreApplication.java
+++ src/main/java/org/humancellatlas/ingest/IngestCoreApplication.java
@@ -1,11 +1,13 @@
 package org.humancellatlas.ingest;
 
+import com.github.cloudyrock.spring.v5.EnableMongock;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
+@EnableMongock
 @SpringBootApplication
 @PropertySources({
         @PropertySource("classpath:application.properties")
