7c252719568a8ea4f8fc952bb7dbdb868f626a00
Aclan Alegria <aaclan@ebi.ac.uk>
2020-10-08 11:30:57 +0100

    Applied PR comments

--- src/main/java/org/humancellatlas/ingest/security/SecurityConfig.java
+++ src/main/java/org/humancellatlas/ingest/security/SecurityConfig.java
@@ -2,9 +2,6 @@ package org.humancellatlas.ingest.security;
 
 import com.auth0.spring.security.api.BearerSecurityContextRepository;
 import com.auth0.spring.security.api.JwtAuthenticationEntryPoint;
-import lombok.NonNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -30,17 +27,17 @@ import static org.springframework.http.HttpMethod.*;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String FORWARDED_HOST = "x-forwarded-host";
 
-    private static final List<AntPathRequestMatcher> SECURED_ANT_PATHS;
+    private static final List<AntPathRequestMatcher> SECURED_ANT_PATHS = setupSecuredAntPaths();
 
-    static {
+    private static final List<AntPathRequestMatcher> SECURED_WRANGLER_ANT_PATHS = setupWranglerAntPaths();
+
+    private static List<AntPathRequestMatcher> setupSecuredAntPaths() {
         List<AntPathRequestMatcher> antPathMatchers = new ArrayList<>();
         antPathMatchers.addAll(defineAntPathMatchers(POST, "/**"));
-        SECURED_ANT_PATHS = Collections.unmodifiableList(antPathMatchers);
+        return Collections.unmodifiableList(antPathMatchers);
     }
 
-    private static final List<AntPathRequestMatcher> SECURED_WRANGLER_ANT_PATHS;
-
-    static {
+    private static List<AntPathRequestMatcher> setupWranglerAntPaths() {
         List<AntPathRequestMatcher> antPathMatchers = new ArrayList<>();
         antPathMatchers.addAll(defineAntPathMatchers(GET, "/bundleManifests"));
         antPathMatchers.addAll(defineAntPathMatchers(GET, "/submissionManifests"));
@@ -55,7 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         antPathMatchers.addAll(defineAntPathMatchers(PUT, "/**"));
         antPathMatchers.addAll(defineAntPathMatchers(PATCH, "/**"));
         antPathMatchers.addAll(defineAntPathMatchers(DELETE, "/**"));
-        SECURED_WRANGLER_ANT_PATHS = Collections.unmodifiableList(antPathMatchers);
+        return Collections.unmodifiableList(antPathMatchers);
     }
 
     private static List<AntPathRequestMatcher> defineAntPathMatchers(HttpMethod method,
@@ -92,22 +89,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(GET, "/user/**").authenticated()
                 .antMatchers(GET, "/auth/account").authenticated()
                 .antMatchers(POST, "/auth/registration").hasAuthority(GUEST.name())
-                .requestMatchers(this::isSecuredWranglerEndpointFromOutside).hasAnyAuthority(WRANGLER.name(), SERVICE.name())
-                .requestMatchers(this::isSecuredEndpointFromOutside).authenticated()
+                .requestMatchers(SecurityConfig::isSecuredWranglerEndpointFromOutside).hasAnyAuthority(WRANGLER.name(), SERVICE.name())
+                .requestMatchers(SecurityConfig::isSecuredEndpointFromOutside).authenticated()
                 .antMatchers(GET, "/**").permitAll();
     }
 
-    private Boolean isSecuredEndpointFromOutside(HttpServletRequest request) {
+    private static Boolean isSecuredEndpointFromOutside(HttpServletRequest request) {
         return SECURED_ANT_PATHS.stream().anyMatch(matcher -> matcher.matches(request)) &&
-                this.isRequestOutsideProxy(request);
+                SecurityConfig.isRequestOutsideProxy(request);
     }
 
-    private Boolean isSecuredWranglerEndpointFromOutside(HttpServletRequest request) {
+    private static Boolean isSecuredWranglerEndpointFromOutside(HttpServletRequest request) {
         return SECURED_WRANGLER_ANT_PATHS.stream().anyMatch(matcher -> matcher.matches(request)) &&
-                this.isRequestOutsideProxy(request);
+                SecurityConfig.isRequestOutsideProxy(request);
     }
 
-    private Boolean isRequestOutsideProxy(HttpServletRequest request) {
+    private static Boolean isRequestOutsideProxy(HttpServletRequest request) {
         return Optional.ofNullable(request.getHeader(FORWARDED_HOST)).isPresent();
     }
 
