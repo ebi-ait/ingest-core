f23d871801aa49871ee2ec78ad2e95ece21cf313
Aclan Alegria <aaclan@ebi.ac.uk>
2020-10-27 14:08:22 +0000

    Extend search by criteria for Protocols

---
+++ src/main/java/org/humancellatlas/ingest/protocol/ProtocolRepositoryImpl.java
@@ -0,0 +1,31 @@
+package org.humancellatlas.ingest.protocol;
+
+import org.humancellatlas.ingest.query.MetadataCriteria;
+import org.humancellatlas.ingest.query.QueryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
+
+public class ProtocolRepositoryImpl implements ProtocolRepositoryCustom {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Autowired
+    private QueryBuilder queryBuilder;
+
+    @Override
+    public Page<Protocol> findByCriteria(List<MetadataCriteria> criteriaList, Boolean andCriteria, Pageable pageable) {
+        Query query = queryBuilder.build(criteriaList, andCriteria);
+
+        long count = mongoTemplate.count(query, Protocol.class);
+        query.with(pageable);
+        List<Protocol> result = mongoTemplate.find(query, Protocol.class);
+
+        return new PageImpl<>(result, pageable, count);
+    };
+}
