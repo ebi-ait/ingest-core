cb7a6639a5ea6fa5184043848a8c06813ae4bd0a
aaclan-ebi <aaclan@ebi.ac.uk>
2020-11-24 17:37:22 +0000

    Updated the CORS Config to not use the deprecated WebMvcConfigurerAdapter

--- src/main/java/org/humancellatlas/ingest/security/CorsConfig.java
+++ src/main/java/org/humancellatlas/ingest/security/CorsConfig.java
@@ -3,20 +3,17 @@ package org.humancellatlas.ingest.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class CorsConfig {
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
-                        .allowedHeaders("*");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("GET", "PATCH", "POST", "PUT", "DELETE").allowedOrigins("*")
+                .allowedHeaders("*");
     }
 }
