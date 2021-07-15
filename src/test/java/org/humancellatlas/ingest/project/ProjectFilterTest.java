685840fa5e320307e27614d9cf2a07fdeefbae27
Amnon Khen <amnon@ebi.ac.uk>
2021-07-15 18:27:15 +0100

    unit test for project service dcp-313

--- src/test/java/org/humancellatlas/ingest/project/ProjectFilterTest.java
+++ src/test/java/org/humancellatlas/ingest/project/ProjectFilterTest.java
@@ -7,7 +7,6 @@ import org.humancellatlas.ingest.project.web.SearchFilter;
 import org.humancellatlas.ingest.schemas.SchemaService;
 import org.humancellatlas.ingest.submission.SubmissionEnvelopeRepository;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +17,10 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.CriteriaDefinition;
 import org.springframework.data.mongodb.core.query.Query;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalUnit;
 import java.util.Comparator;
 import java.util.Map;
 
@@ -61,8 +58,10 @@ class ProjectFilterTest {
     private ProjectEventHandler projectEventHandler;
 
     private Project project1;
+    private Project project2;
+    private Project project3;
 
-    Comparator<Instant> closeEnough = new Comparator<Instant>() {
+    Comparator<Instant> upToMillies = new Comparator<Instant>() {
         public int compare(Instant d1, Instant d2) {
             return d1.truncatedTo(ChronoUnit.MILLIS).compareTo(d2.truncatedTo(ChronoUnit.MILLIS));
         }
@@ -71,9 +70,9 @@ class ProjectFilterTest {
     @Test
     void test_raw_criteria() {
         Query query = new Query().addCriteria(Criteria.where("content.project_core.project_title").regex("project1", "i"));
-        Project actual = this.mongoTemplate.find(query, Project.class).get(0);
+        Project actual = this.mongoTemplate.findOne(query, Project.class);
         assertThat(actual)
-                .usingComparatorForFields(closeEnough,"contentLastModified")
+                .usingComparatorForFields(upToMillies, "contentLastModified")
                 .isEqualToComparingFieldByFieldRecursively(project1);
 
     }
@@ -82,16 +81,46 @@ class ProjectFilterTest {
     void test_criteria_building() {
         SearchFilter searchFilter = new SearchFilter("project1", null, null);
         Query query = projectService.buildProjectsQuery(searchFilter);
-        Project actual = this.mongoTemplate.find(query, Project.class).get(0);
+        Project actual = this.mongoTemplate.findOne(query, Project.class);
         assertThat(actual)
-                .usingComparatorForFields(closeEnough,"contentLastModified")
+                .usingComparatorForFields(upToMillies, "contentLastModified")
                 .isEqualToComparingFieldByFieldRecursively(project1);
+    }
 
+    @Test
+    void filter_by_state() {
+        Project project4 = makeProject("project4");
+        project4.setWranglingState(WranglingState.IN_PROGRESS);
+
+        //when
+        SearchFilter filterNew = new SearchFilter(null, "New", null);
+
+        Pageable pageable = PageRequest.of(1, 10);
+        Page<Project> result = projectService.filterProjects(filterNew, pageable);
+
+        // then
+        assertThat(result.getTotalElements()).isEqualTo(1);
+        assertThat(result.getContent()).hasSize(3);
+        assertThat(result.getContent()).containsExactlyInAnyOrder(project1, project2, project3);
+    }
+
+    @Test
+    void filter_by_wrnagler() {
+        // given
+        //when
+        SearchFilter searchFilter = new SearchFilter(null, null, "wrangler_project2");
+
+        Pageable pageable = PageRequest.of(1, 10);
+        Page<Project> result = projectService.filterProjects(searchFilter, pageable);
+
+        // then
+        assertThat(result.getTotalElements()).isEqualTo(1);
+        assertThat(result.getContent()).hasSize(1);
+        assertThat(result.getContent()).containsExactly(this.project2);
     }
 
     @Test
     void filter_by_text() {
-
         // given
         //when
         SearchFilter searchFilter = new SearchFilter("project1", null, null);
@@ -110,6 +139,8 @@ class ProjectFilterTest {
                 Map.of("project_title", title));
         Project project = new Project(content);
         project.setIsUpdate(false);
+        project.setPrimaryWrangler("wrangler_" + title);
+        project.setWranglingState(WranglingState.NEW);
         return project;
     }
 
@@ -127,9 +158,11 @@ class ProjectFilterTest {
                 projectEventHandler);
         assertThat(this.mongoTemplate.findAll(Project.class)).hasSize(0);
         this.project1 = makeProject("project1");
+        this.project2 = makeProject("project2");
+        this.project3 = makeProject("project3");
         this.mongoTemplate.save(project1);
-        this.mongoTemplate.save(makeProject("project2"));
-        this.mongoTemplate.save(makeProject("project3"));
+        this.mongoTemplate.save(project2);
+        this.mongoTemplate.save(project3);
         assertThat(this.mongoTemplate.findAll(Project.class)).hasSize(3);
     }
 
