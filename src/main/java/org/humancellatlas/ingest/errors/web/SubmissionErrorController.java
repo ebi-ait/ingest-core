42cd985c60b904172a4e09288db2f15240ecadf1
Alegria Aclan <aaclan@ebi.ac.uk>
2021-08-13 21:31:58 +0800

    Update src/main/java/org/humancellatlas/ingest/errors/web/SubmissionErrorController.java
    Co-authored-by: Károly Erdős <2183410+ke4@users.noreply.github.com>

--- src/main/java/org/humancellatlas/ingest/errors/web/SubmissionErrorController.java
+++ src/main/java/org/humancellatlas/ingest/errors/web/SubmissionErrorController.java
@@ -16,7 +16,11 @@ import org.springframework.hateoas.ExposesResourceFor;
 import org.springframework.hateoas.PagedResources;
 import org.springframework.hateoas.Resource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.net.URI;
 
