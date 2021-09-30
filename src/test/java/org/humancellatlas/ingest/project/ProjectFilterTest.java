9653b83284a5ffd7aa180fbf41caa08c171131a9
Jacob Windsor <me@jcbwndsr.com>
2021-09-30 11:40:27 +0100

    Fix constructor ordering

--- src/test/java/org/humancellatlas/ingest/project/ProjectFilterTest.java
+++ src/test/java/org/humancellatlas/ingest/project/ProjectFilterTest.java
@@ -378,9 +378,9 @@ class ProjectFilterTest {
                 false,
                 "Human",
                 "AN_ONTOLOGY_TERM",
-                SearchType.AllKeywords,
                 0,
-                10000
+                10000,
+                SearchType.AllKeywords
         );
     }
 
