ecffe6b4c688f9a6ff2f46e2492ed917a89e5c72
Jacob Windsor <me@jcbwndsr.com>
2021-10-01 13:14:31 +0100

    Remove serializer. Why was it there? :/

--- src/main/java/org/humancellatlas/ingest/project/DataAccessTypes.java
+++ src/main/java/org/humancellatlas/ingest/project/DataAccessTypes.java
@@ -1,10 +1,7 @@
 package org.humancellatlas.ingest.project;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Getter;
-import org.humancellatlas.ingest.archiving.entity.ArchiveEntityTypeSerializer;
 
-@JsonSerialize(using = ArchiveEntityTypeSerializer.class)
 public enum DataAccessTypes {
     OPEN("All fully open"),
     MANAGED("All managed access"),
