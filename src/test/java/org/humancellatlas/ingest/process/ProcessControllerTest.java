498e0ceae188ede3901680aa9af97d12f6b60c98
Yusra Haider <yhaider@ebi.ac.uk>
2021-11-10 16:04:50 +0000

    ebi-ait/dcp-ingest-central#517: fixed test for delete protocol linking to process

--- src/test/java/org/humancellatlas/ingest/process/ProcessControllerTest.java
+++ src/test/java/org/humancellatlas/ingest/process/ProcessControllerTest.java
@@ -1,7 +1,10 @@
-package org.humancellatlas.ingest.process.web;
+package org.humancellatlas.ingest.process;
 
 import org.humancellatlas.ingest.config.MigrationConfiguration;
 import org.humancellatlas.ingest.core.service.ValidationStateChangeService;
+import org.humancellatlas.ingest.messaging.MessageRouter;
+import org.humancellatlas.ingest.protocol.Protocol;
+import org.humancellatlas.ingest.protocol.ProtocolRepository;
 import org.humancellatlas.ingest.state.ValidationState;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +12,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 
@@ -22,22 +25,38 @@ class ProcessControllerTest {
     @MockBean
     ValidationStateChangeService validationStateChangeService;
 
-    @Autowired
-    private MockMvc webApp;
+    @MockBean
+    private MessageRouter messageRouter;
 
     @MockBean
     private MigrationConfiguration migrationConfiguration;
 
+    @Autowired
+    private MockMvc webApp;
+
+    @Autowired
+    private ProcessRepository processRepository;
+
+    @Autowired
+    private ProtocolRepository protocolRepository;
+
+
    @Test
    public void testDeleteTriggersValidationStateToDraft() throws Exception {
-       // send delete request
-       String processId = "testProcess";
-       String protocolId = "testProtocol";
+       // given
+       Protocol protocol = new Protocol("protocol");
+       protocolRepository.save(protocol);
 
-       MvcResult result = webApp
-               .perform(delete("/processes/{processId}/protocols/{protocolId}", processId, protocolId))
-               .andReturn();
-       // verify service being called
-       verify(validationStateChangeService).changeValidationState(any(),any(), eq(ValidationState.DRAFT));
+       // and
+       Process process = new Process("process");
+       process.addProtocol(protocol);
+       processRepository.save(process);
+
+       // send delete request
+       webApp.perform(delete("/processes/{processId}/protocols/{protocolId}", process.getId(), protocol.getId()));
+      // verify service being called
+       verify(validationStateChangeService, times(1)).changeValidationState(any(),any(), eq(ValidationState.DRAFT));
    }
+
+
 }
