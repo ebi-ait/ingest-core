d87a569ab1cdfcc95979bffcfcd6d955aa9c3c88
Yusra Haider <yhaider@ebi.ac.uk>
2021-11-11 03:22:19 +0000

    ebi-ait/dcp-ingest-central#517: added FileControllerTest

---
+++ src/test/java/org/humancellatlas/ingest/file/FileControllerTest.java
@@ -0,0 +1,114 @@
+package org.humancellatlas.ingest.file;
+
+import org.humancellatlas.ingest.config.MigrationConfiguration;
+import org.humancellatlas.ingest.core.service.ValidationStateChangeService;
+import org.humancellatlas.ingest.messaging.MessageRouter;
+import org.humancellatlas.ingest.process.Process;
+import org.humancellatlas.ingest.process.ProcessRepository;
+import org.humancellatlas.ingest.state.ValidationState;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc(printOnlyOnFailure = false)
+public class FileControllerTest {
+    @MockBean
+    ValidationStateChangeService validationStateChangeService;
+
+    @Autowired
+    private MockMvc webApp;
+
+    @Autowired
+    private ProcessRepository processRepository;
+
+    @Autowired
+    private FileRepository fileRepository;
+
+    @MockBean
+    private MigrationConfiguration migrationConfiguration;
+
+    @MockBean
+    private MessageRouter messageRouter;
+
+    @Test
+    public void testDeleteInputToProcessTriggersValidationStateToDraft() throws Exception {
+        // given
+        Process process = new Process("process");
+        processRepository.save(process);
+
+        // and
+        File file = new File();
+        file.addAsInputToProcess(process);
+        fileRepository.save(file);
+
+        // send delete request
+        webApp.perform(delete("/files/{fileId}/inputToProcesses/{processId}", file.getId(), process.getId()));
+        // verify service being called
+        verify(validationStateChangeService, times(1)).changeValidationState(any(),any(), eq(ValidationState.DRAFT));
+    }
+
+    @Test
+    public void testSaveInputToProcessTriggersValidationStateToDraft() throws Exception {
+        // given
+        Process process = new Process("process1");
+        processRepository.save(process);
+
+        // and
+        File file = new File();
+        fileRepository.save(file);
+
+        // send post request
+        webApp.perform(post("/files/{fileId}/inputToProcesses/", file.getId())
+                .contentType("text/uri-list")
+                .content(ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString() + "/processes/" + process.getId()));
+        // verify service being called
+        verify(validationStateChangeService, times(1)).changeValidationState(any(),any(), eq(ValidationState.DRAFT));
+    }
+
+    @Test
+    public void testDeleteDerivedByProcessTriggersValidationStateToDraft() throws Exception {
+        // given
+        Process process = new Process("process2");
+        processRepository.save(process);
+
+        // and
+        File file = new File();
+        file.addAsInputToProcess(process);
+        fileRepository.save(file);
+
+        // send delete request
+        webApp.perform(delete("/files/{fileId}/derivedByProcesses/{processId}", file.getId(), process.getId()));
+        // verify service being called
+        verify(validationStateChangeService, times(1)).changeValidationState(any(),any(), eq(ValidationState.DRAFT));
+    }
+
+    @Test
+    public void testDerivedByProcessTriggersValidationStateToDraft() throws Exception {
+        // given
+        Process process = new Process("process3");
+        processRepository.save(process);
+
+        // and
+        File file = new File();
+        fileRepository.save(file);
+
+        // send post request
+        webApp.perform(post("/files/{fileId}/derivedByProcesses/", file.getId())
+                .contentType("text/uri-list")
+                .content(ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString() + "/processes/" + process.getId()));
+        // verify service being called
+        verify(validationStateChangeService, times(1)).changeValidationState(any(),any(), eq(ValidationState.DRAFT));
+    }
+}
