f69cc3b8da73e6174ef65682f45e45a1c94c529d
Jacob Windsor <me@jcbwndsr.com>
2021-11-19 12:50:30 +0100

    Add affected entitites

--- src/main/java/org/humancellatlas/ingest/submission/GraphValidationError.java
+++ src/main/java/org/humancellatlas/ingest/submission/GraphValidationError.java
@@ -1,8 +1,18 @@
+// Temporary classes until dcp-507 is done
+
 package org.humancellatlas.ingest.submission;
 import lombok.Data;
+
+@Data
+class AffectedEntity {
+    private String[] types;
+    private String uuid;
+}
 
 @Data
 public class GraphValidationError {
     private String test;
     private String message;
+    private AffectedEntity[] affectedEntities;
 }
+ 
