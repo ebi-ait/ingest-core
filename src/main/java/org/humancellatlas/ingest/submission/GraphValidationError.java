8a5106e11bf0d06f19db3398e428d1d854e8a415
Jacob Windsor <me@jcbwndsr.com>
2021-11-19 13:51:14 +0100

    Use plain object list

--- src/main/java/org/humancellatlas/ingest/submission/GraphValidationError.java
+++ src/main/java/org/humancellatlas/ingest/submission/GraphValidationError.java
@@ -4,15 +4,9 @@ package org.humancellatlas.ingest.submission;
 import lombok.Data;
 
 @Data
-class AffectedEntity {
-    private String[] types;
-    private String uuid;
-}
-
-@Data
 public class GraphValidationError {
     private String test;
     private String message;
-    private AffectedEntity[] affectedEntities;
+    private Object[] affectedEntities;
 }
  
