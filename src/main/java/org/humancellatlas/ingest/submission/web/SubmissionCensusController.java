b48a3d3ac5ced52fabec74ca3cbc5d6941fb3856
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2021-11-22 15:05:38 +0000

    transform processes into ids

--- src/main/java/org/humancellatlas/ingest/submission/web/SubmissionCensusController.java
+++ src/main/java/org/humancellatlas/ingest/submission/web/SubmissionCensusController.java
@@ -1,16 +1,19 @@
 package org.humancellatlas.ingest.submission.web;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.humancellatlas.ingest.biomaterial.BiomaterialRepository;
+import org.humancellatlas.ingest.core.AbstractEntity;
 import org.humancellatlas.ingest.file.FileRepository;
+import org.humancellatlas.ingest.process.Process;
 import org.humancellatlas.ingest.process.ProcessRepository;
 import org.humancellatlas.ingest.protocol.ProtocolRepository;
 import org.humancellatlas.ingest.submission.SubmissionEnvelope;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.UUID;
 
 @RestController
@@ -29,18 +32,23 @@ public class SubmissionCensusController {
     @RequestMapping(path = "/submissionEnvelopes/{sub_id}/census", method = RequestMethod.GET)
     @ResponseBody
     public SubmissionCensus submissionCensus(@PathVariable("sub_id") SubmissionEnvelope submissionEnvelope) {
-        SubmissionCensus submissionCensus = new SubmissionCensus();
-        submissionCensus.setUuid(submissionEnvelope.getUuid().getUuid());
-
-        return submissionCensus;
+        return new SubmissionCensus(submissionEnvelope);
     }
 
     @Getter
-    @Setter
-    @NoArgsConstructor
-    public static class SubmissionCensus {
-        private UUID uuid;
+    public class SubmissionCensus {
+        private final UUID uuid;
+        private final Dictionary<String, UUID> processes;
 
+        public SubmissionCensus(SubmissionEnvelope submissionEnvelope){
+            this.uuid = submissionEnvelope.getUuid().getUuid();
+            this.processes = new Hashtable<>();
+            Collection<Process> allProcesses = processRepository.findAllBySubmissionEnvelope(submissionEnvelope);
+            allProcesses.forEach(process -> this.setDictionary(this.processes, process));
+        }
+
+        private void setDictionary(Dictionary<String, UUID> dictionary, AbstractEntity entity){
+            dictionary.put(entity.getId(), entity.getUuid().getUuid());
+        }
     }
-    
 }
