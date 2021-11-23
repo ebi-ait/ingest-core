c67237da75616557fdb0900ff3dae82732ae04cc
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2021-11-23 10:40:41 +0000

    relationship mapping for biomaterial, remove exta mapping from process

--- src/main/java/org/humancellatlas/ingest/submission/web/SubmissionCensusController.java
+++ src/main/java/org/humancellatlas/ingest/submission/web/SubmissionCensusController.java
@@ -1,6 +1,7 @@
 package org.humancellatlas.ingest.submission.web;
 
 import lombok.Getter;
+import org.humancellatlas.ingest.biomaterial.Biomaterial;
 import org.humancellatlas.ingest.biomaterial.BiomaterialRepository;
 import org.humancellatlas.ingest.file.FileRepository;
 import org.humancellatlas.ingest.process.Process;
@@ -36,7 +37,7 @@ public class SubmissionCensusController {
         private final UUID uuid;
         private final Dictionary<String, ProcessCensus> processes = new Hashtable<>();
         private final Dictionary<String, UUID> protocols = new Hashtable<>();
-        private final Dictionary<String, UUID> biomaterials = new Hashtable<>();
+        private final Dictionary<String, BiomaterialCensus> biomaterials = new Hashtable<>();
         private final Dictionary<String, UUID> files = new Hashtable<>();
 
         public SubmissionCensus(SubmissionEnvelope submissionEnvelope){
@@ -49,7 +50,7 @@ public class SubmissionCensusController {
                 .forEach(protocol -> this.protocols.put(protocol.getId(), protocol.getUuid().getUuid()));
             biomaterialRepository
                 .findBySubmissionEnvelope(submissionEnvelope)
-                .forEach(biomaterial -> this.biomaterials.put(biomaterial.getId(), biomaterial.getUuid().getUuid()));
+                .forEach(biomaterial -> this.biomaterials.put(biomaterial.getId(), new BiomaterialCensus(biomaterial)));
             fileRepository
                 .findBySubmissionEnvelope(submissionEnvelope)
                 .forEach(file -> this.files.put(file.getId(), file.getUuid().getUuid()));
@@ -60,18 +61,23 @@ public class SubmissionCensusController {
     public class ProcessCensus {
         private final UUID uuid;
         private final Collection<String> protocols = new HashSet<>();
-        private final Collection<String> inputBiomaterials = new HashSet<>();
-        private final Collection<String> derivedBiomaterials = new HashSet<>();
-        private final Collection<String> inputFiles = new HashSet<>();
-        private final Collection<String> derivedFiles = new HashSet<>();
 
         public ProcessCensus(Process process) {
             this.uuid = process.getUuid().getUuid();
             process.getProtocols().forEach(protocol -> this.protocols.add(protocol.getId()));
-            biomaterialRepository.findByInputToProcessesContains(process).forEach(biomaterial -> this.inputBiomaterials.add(biomaterial.getId()));
-            biomaterialRepository.findByDerivedByProcessesContains(process).forEach(biomaterial -> this.derivedBiomaterials.add(biomaterial.getId()));
-            fileRepository.findByInputToProcessesContains(process).forEach(file -> this.inputFiles.add(file.getId()));
-            fileRepository.findByDerivedByProcessesContains(process).forEach(file -> this.derivedFiles.add(file.getId()));
+        }
+    }
+
+    @Getter
+    public class BiomaterialCensus {
+        private final UUID uuid;
+        private final Collection<String> derivedByProcesses = new HashSet<>();
+        private final Collection<String> inputToProcesses = new HashSet<>();
+
+        public BiomaterialCensus(Biomaterial biomaterial) {
+            this.uuid = biomaterial.getUuid().getUuid();
+            biomaterial.getDerivedByProcesses().forEach(process -> this.derivedByProcesses.add(process.getId()));
+            biomaterial.getInputToProcesses().forEach(process -> this.inputToProcesses.add(process.getId()));
         }
     }
 }
