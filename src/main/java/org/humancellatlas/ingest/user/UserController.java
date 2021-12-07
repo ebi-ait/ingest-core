0093b7cc7227f1e0df2600eb655e802f09bf170e
Jacob Windsor <me@jcbwndsr.com>
2021-12-07 17:59:44 +0100

    Remove unused vars

--- src/main/java/org/humancellatlas/ingest/user/UserController.java
+++ src/main/java/org/humancellatlas/ingest/user/UserController.java
@@ -71,12 +71,6 @@ public class UserController implements ResourceProcessor<RepositoryLinksResource
         String user = getCurrentAccount().getId();
         long pendingSubmissions = submissionEnvelopeRepository.countBySubmissionStateAndUser(SubmissionState.PENDING, user);
         long draftSubmissions = submissionEnvelopeRepository.countBySubmissionStateAndUser(SubmissionState.DRAFT, user);
-        long validatingubmissions = submissionEnvelopeRepository.countBySubmissionStateAndUser(SubmissionState.METADATA_VALIDATING, user);
-        long validSubmissions = submissionEnvelopeRepository.countBySubmissionStateAndUser(SubmissionState.METADATA_VALID, user);
-        long invalidSubmissions = submissionEnvelopeRepository.countBySubmissionStateAndUser(SubmissionState.METADATA_INVALID, user);
-        long submittedSubmissions = submissionEnvelopeRepository.countBySubmissionStateAndUser(SubmissionState.SUBMITTED, user);
-        long processingSubmissions = submissionEnvelopeRepository.countBySubmissionStateAndUser(SubmissionState.PROCESSING, user);
-        long cleanupSubmissions = submissionEnvelopeRepository.countBySubmissionStateAndUser(SubmissionState.CLEANUP, user);
         long completedSubmissions = submissionEnvelopeRepository.countBySubmissionStateAndUser(SubmissionState.COMPLETE, user);
         long projects = projectRepository.countByUser(user);
         return new Summary(pendingSubmissions, draftSubmissions, completedSubmissions, projects);
