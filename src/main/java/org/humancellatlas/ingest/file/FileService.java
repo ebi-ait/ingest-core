0394f284d9b396e70d7ac45cbd7ae723b3a8e8a2
Jacob Windsor <me@jcbwndsr.com>
2022-01-19 14:20:20 +0000

    Enact state transition using service so that validator is notified

--- src/main/java/org/humancellatlas/ingest/file/FileService.java
+++ src/main/java/org/humancellatlas/ingest/file/FileService.java
@@ -5,10 +5,12 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.humancellatlas.ingest.biomaterial.BiomaterialRepository;
 import org.humancellatlas.ingest.core.Checksums;
+import org.humancellatlas.ingest.core.EntityType;
 import org.humancellatlas.ingest.core.Uuid;
 import org.humancellatlas.ingest.core.exception.CoreEntityNotFoundException;
 import org.humancellatlas.ingest.core.service.MetadataCrudService;
 import org.humancellatlas.ingest.core.service.MetadataUpdateService;
+import org.humancellatlas.ingest.core.service.ValidationStateChangeService;
 import org.humancellatlas.ingest.file.web.FileMessage;
 import org.humancellatlas.ingest.process.ProcessRepository;
 import org.humancellatlas.ingest.state.MetadataDocumentEventHandler;
@@ -51,6 +53,8 @@ public class FileService {
     MetadataCrudService metadataCrudService;
     private final @NonNull
     MetadataUpdateService metadataUpdateService;
+    private final @NonNull
+    ValidationStateChangeService validationStateChangeService;
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
@@ -112,8 +116,10 @@ public class FileService {
         file.setChecksums(checksums);
         file.setSize(size);
         file.setFileContentType(contentType);
-        file.enactStateTransition(ValidationState.DRAFT);
         File updatedFile = fileRepository.save(file);
+
+        validationStateChangeService.changeValidationState(EntityType.FILE, updatedFile.getId(), ValidationState.DRAFT);
+
         return updatedFile;
     }
 
