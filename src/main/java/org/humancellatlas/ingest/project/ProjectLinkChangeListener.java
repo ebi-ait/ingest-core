11ddab9e01d48e4cc0ebce5db62773ff557b3471
Amnon Khen <amnon@ebi.ac.uk>
2022-09-02 11:06:48 +0100

    dcp-627 project status updates - test

---
+++ src/main/java/org/humancellatlas/ingest/project/ProjectLinkChangeListener.java
@@ -0,0 +1,48 @@
+package org.humancellatlas.ingest.project;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.humancellatlas.ingest.core.MetadataDocument;
+import org.humancellatlas.ingest.submission.SubmissionEnvelope;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.rest.core.annotation.HandleBeforeLinkSave;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Set;
+
+@Component
+@RepositoryEventHandler
+@RequiredArgsConstructor
+public class ProjectLinkChangeListener {
+
+    private final @NonNull Logger log = LoggerFactory.getLogger(getClass());
+
+    /**
+     * The `linked` parameter, due to a bug in Spring, is passed to the handler as a {@link java.lang.reflect.Proxy}
+     * object. This is a proxy to a {@link Collection}. Since we need to respond to associations of
+     * {@link SubmissionEnvelope}s and not other properties of {@link Project}, we need to filter the contents
+     * of the `linked` Collection.
+     *
+     * @link <a href="https://stackoverflow.com/questions/70288400/spring-repositoryeventhandler-for-link-modification-missing-link-target-referenc">Stack Overflow ticket</a>
+     */
+    @HandleBeforeLinkSave
+    public void beforeLinkSaved(Project project, Object linked) {
+        if (linked instanceof Collection) {
+            Collection<?> linkedCollection = (Collection<?>) linked;
+            if (isSubmissionEnvelopeCollection(linkedCollection)) {
+                log.info("setting project {} to IN_PROGRESS", project.getUuid().getUuid().toString());
+                project.setWranglingState(WranglingState.IN_PROGRESS);
+            }
+
+        }
+    }
+
+    private boolean isSubmissionEnvelopeCollection(Collection collection) {
+        Object o = collection.stream().findFirst().get();
+        return o instanceof SubmissionEnvelope;
+    }
+
+}
