024f3dfac0c76387b1660ec9008f6327c366348c
Amnon Khen <amnon@ebi.ac.uk>
2023-03-27 15:21:36 +0100

    dcp-ingest-central-926 large dataset crashes spreadsheet generation

--- src/main/java/org/humancellatlas/ingest/submission/web/SubmissionLinkMapController.java
+++ src/main/java/org/humancellatlas/ingest/submission/web/SubmissionLinkMapController.java
@@ -1,18 +1,25 @@
 package org.humancellatlas.ingest.submission.web;
 
 import lombok.Getter;
+import lombok.NonNull;
 import org.humancellatlas.ingest.biomaterial.Biomaterial;
 import org.humancellatlas.ingest.biomaterial.BiomaterialRepository;
+import org.humancellatlas.ingest.core.web.Links;
 import org.humancellatlas.ingest.file.File;
 import org.humancellatlas.ingest.file.FileRepository;
 import org.humancellatlas.ingest.process.Process;
 import org.humancellatlas.ingest.process.ProcessRepository;
 import org.humancellatlas.ingest.protocol.ProtocolRepository;
 import org.humancellatlas.ingest.submission.SubmissionEnvelope;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.Pair;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
+
+import static java.util.stream.Collectors.*;
 
 @RestController
 public class SubmissionLinkMapController {
@@ -26,8 +33,11 @@ public class SubmissionLinkMapController {
     @Autowired
     ProtocolRepository protocolRepository;
 
+    @NonNull
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
-    @RequestMapping(path = "/submissionEnvelopes/{sub_id}/linkingMap", method = RequestMethod.GET)
+    @RequestMapping(path = "/submissionEnvelopes/{sub_id}"+ Links.SUBMISSION_LINKING_MAP_URL,
+            method = RequestMethod.GET)
     @ResponseBody
     public SubmissionLinkingMap getSubmissionLinkMap(@PathVariable("sub_id") SubmissionEnvelope submissionEnvelope) {
         return new SubmissionLinkingMap(submissionEnvelope);
@@ -41,16 +51,48 @@ public class SubmissionLinkMapController {
         final Dictionary<String, FileLinkingMap> files = new Hashtable<>();
 
         public SubmissionLinkingMap(SubmissionEnvelope submissionEnvelope){
-            processRepository
-                .findBySubmissionEnvelope(submissionEnvelope)
-                .forEach(process -> this.processes.put(process.getId(), new ProcessLinkingMap(process)));
+            log.info("before processes");
+            oldImplementation(submissionEnvelope);
+            log.info("found {} processes", processes.size());
+            log.info("before biomaterials");
             biomaterialRepository
                 .findBySubmissionEnvelope(submissionEnvelope)
                 .forEach(biomaterial -> this.biomaterials.put(biomaterial.getId(), new BiomaterialLinkingMap(biomaterial)));
+            log.info("found {} biomaterials", biomaterials.size());
+            log.info("before files");
             fileRepository
                 .findBySubmissionEnvelope(submissionEnvelope)
                 .forEach(file -> this.files.put(file.getId(), new FileLinkingMap(file)));
+            log.info("found {} files", files.size());
         }
+
+        private void oldImplementation(SubmissionEnvelope submissionEnvelope) {
+            processRepository
+                .findBySubmissionEnvelope(submissionEnvelope)
+                .forEach(process -> this.processes.put(process.getId(), new ProcessLinkingMap(process)));
+        }
+
+        void newImplementation(SubmissionEnvelope submissionEnvelope) {
+            /// each is grouped by process
+            /// TODO merge by process ID
+            biomaterialRepository.findBySubmissionEnvelope(submissionEnvelope)
+                    .flatMap(biomaterial->biomaterial.getInputToProcesses()
+                                                        .stream()
+                                                        .map(process->Pair.of(biomaterial.getId(), process.getId())))
+                    .collect(groupingBy(Pair::getSecond, mapping(Pair::getFirst, toSet())));
+            fileRepository.findBySubmissionEnvelope(submissionEnvelope)
+                    .flatMap(file->file.getInputToProcesses()
+                            .stream()
+                            .map(process->Pair.of(file.getId(), process.getId())))
+                    .collect(groupingBy(Pair::getSecond, mapping(Pair::getFirst, toSet())));
+            processRepository
+                    .findBySubmissionEnvelope(submissionEnvelope)
+                    .map(process -> Pair.of(process.getId(), process.getProtocols()))
+                    .collect(groupingBy(Pair::getFirst, mapping(Pair::getSecond, toSet())));
+
+
+        }
+
     }
 
     @Getter
