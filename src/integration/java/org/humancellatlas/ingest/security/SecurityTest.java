8389a058bb89c4012f471293d3bcb4d31175e128
Amnon Khen <amnon@ebi.ac.uk>
2023-10-04 08:49:02 +0100

    dcp-967 managed access - require authentication for all read operations

--- src/integration/java/org/humancellatlas/ingest/security/SecurityTest.java
+++ src/integration/java/org/humancellatlas/ingest/security/SecurityTest.java
@@ -2,7 +2,6 @@ package org.humancellatlas.ingest.security;
 
 import org.humancellatlas.ingest.config.MigrationConfiguration;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
