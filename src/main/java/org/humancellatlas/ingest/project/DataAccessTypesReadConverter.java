5ca8b6afb362cf98910dbf76ebf2c59983eb5755
Amnon Khen <amnon@ebi.ac.uk>
2023-10-04 08:48:57 +0100

    dcp-967 (de)serialisation of DataAccessTypes

--- src/main/java/org/humancellatlas/ingest/project/DataAccessTypesReadConverter.java
+++ src/main/java/org/humancellatlas/ingest/project/DataAccessTypesReadConverter.java
@@ -2,11 +2,13 @@ package org.humancellatlas.ingest.project;
 
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.convert.ReadingConverter;
+import org.springframework.lang.Nullable;
 
 @ReadingConverter()
 public class DataAccessTypesReadConverter implements Converter<String, Object> {
 
     @Override
+    @Nullable
     public Object convert(String source) {
         return DataAccessTypes.valueOf(source);
     }
