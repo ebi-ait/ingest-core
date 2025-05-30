cbf8f0e79cc079d5cedea266f97446dc0b2b0ee6
Alexandros Orges Koci <koci.orges@hotmail.com>
2025-05-30 12:14:55 +0100

    Implemented StudyResourceProcessor to enrich Study HAL links

---
+++ src/main/java/uk/ac/ebi/subs/ingest/study/web/StudyResourceProcessor.java
@@ -0,0 +1,52 @@
+package uk.ac.ebi.subs.ingest.study.web;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceProcessor;
+import org.springframework.stereotype.Component;
+import uk.ac.ebi.subs.ingest.study.Study;
+
+@Component
+@RequiredArgsConstructor
+public class StudyResourceProcessor implements ResourceProcessor<Resource<Study>> {
+
+    private final @NonNull EntityLinks entityLinks;
+
+    @Override
+    public Resource<Study> process(Resource<Study> resource) {
+        Study study = resource.getContent();
+
+        if (study != null && study.getId() != null) {
+            resource.add(rawDatasetsLink(study));
+            resource.add(processedDatasetsLink(study));
+            resource.add(analysisDatasetsLink(study));
+        }
+
+        return resource;
+    }
+
+    private Link rawDatasetsLink(Study study) {
+        return new Link(
+                "/datasets/search/byStudyAndType?id=" + study.getId() + "&type=raw",
+                "rawDatasets"
+        ).withTitle("Raw datasets for this study");
+    }
+
+    private Link processedDatasetsLink(Study study) {
+        return new Link(
+                "/datasets/search/byStudyAndType?id=" + study.getId() + "&type=processed",
+                "processedDatasets"
+        ).withTitle("Processed datasets for this study");
+    }
+
+    private Link analysisDatasetsLink(Study study) {
+        return new Link(
+                "/datasets/search/byStudyAndType?id=" + study.getId() + "&type=analysis",
+                "analysisDatasets"
+        ).withTitle("Analysis datasets for this study");
+    }
+
+}
